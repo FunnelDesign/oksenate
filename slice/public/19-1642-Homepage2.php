c97dfc5d5c2a932f62c77b871f541cfaf9318253
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-05 14:28:53 +0300

    main

--- slice/public/19-1642-Homepage2.php
+++ slice/public/19-1642-Homepage2.php
@@ -34,10 +34,64 @@
       </div>
 
       <div class="bContainer">
+        <div class="sFinder__in">
+          <div class="sFinder__col">
 
+            <div class="sFinder__ico">
+              <img src="../dist/images/tmp/finder-ico-img-1.png" srcset="../dist/images/tmp/finder-ico-img-1-2x.png 2x"  alt="">
+            </div>
+            <h3>Find My Senator</h3>
+            <div class="sFinder__desc">
+              <p>We’ve made it easy to contact your senator.</p>
+            </div>
+            <div class="sFinder__btn-wrap">
+              <a href="#" class="btn">find your senator here</a>
+            </div>
 
+          </div>
+          <div class="sFinder__col">
+
+            <div class="sFinder__ico">
+              <img src="../dist/images/tmp/finder-ico-img-2.png" srcset="../dist/images/tmp/finder-ico-img-2-2x.png 2x"  alt="">
+            </div>
+            <h3>Find Legislation</h3>
+            <div class="sFinder__desc">
+              <p>If you know the bill number, enter it below.</p>
+            </div>
+            <div class="form f-search">
+              <div class="form-item">
+                <input type="text" class="form-text" placeholder="Enter Bill Number"/>
+              </div>
+              <div class="form-actions">
+                <input type="submit" class="form-submit" value="search">
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
+
+    <section class="section section_bg-color_a section_ind_a">
+
+      <div class="bContainer">
+        <div class="bTiles">
+          <div class="bTiles__items">
+
+            <div class="bTiles__item">
+
+            </div>
+            <div class="bTiles__item">
+
+            </div>
+            <div class="bTiles__item">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
 
   </div>
   <?php include 'tpl/blocks/sFooter.inc'; ?>
--- slice/src/styles.js
+++ slice/src/styles.js
@@ -4,6 +4,10 @@ import './common/global.scss';
 import './common/grid.scss';
 import './common/helpers.scss';
 
+
+import './components/form/form.scss'
+import './components/form/f-search.scss'
+import './components/btn/btn.scss'
 import './components/section/section.scss'
 import './components/pMain/pMain.scss'
 import './components/pageWr/pageWr.scss'
@@ -14,6 +18,7 @@ import './components/logo/logo.scss'
 import './components/navLinks/navLinks.scss'
 import './components/bSeacrh/bSearch.scss'
 import './components/sHero/sHero.scss'
-
+import './components/sFinder/sFinder.scss'
+import './components/bTiles/bTiles.scss'
 
 
