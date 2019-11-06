8730a1babceee3318c6db3d26a8979e4ec7b9ea9
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-06 16:14:23 +0300

    added fonts

--- slice/src/styles.js
+++ slice/src/styles.js
@@ -24,4 +24,5 @@ import './components/bEvent/bEvent.scss'
 import './components/sLead/sLead.scss'
 import './components/bCols/bCols.scss'
 import './components/senators/senators.scss'
+import './components/bMessage/bMessage.scss'
 
