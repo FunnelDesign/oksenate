eb71574a73f4b0f5675679c137d5f87f82cc8dae
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-08 18:52:53 +0300

    updated header

--- slice/src/components/Observer/CustomEv.js
+++ slice/src/components/Observer/CustomEv.js
@@ -16,6 +16,22 @@ class CustomEv {
 		$elm.on(`ev.removeClass.${clName}`, (e) => {
 			$('body').removeClass(`${clName}-open`);
 		});
+
+		var ro = new ResizeObserver( entries => {
+			for (let entry of entries) {
+				const cr = entry.contentRect;
+				console.log('Element:', entry.target);
+				console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+				console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+
+				if ($elm.hasClass(`sHeader_fixed`)) {
+					$('.pageWr').css('padding-top', `${cr.height}px`)
+				}
+			}
+		});
+
+// Observe one or multiple elements
+		ro.observe($elm[0]);
 	}
 
 	addAccessibilityCookie() {
