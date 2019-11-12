1e620e9b82a9e50ce29bd5e0c6bf9fa0e776a1de
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-12 17:01:50 +0300

    #111637 Create blank pages

---
+++ slice/public/Page-not-complete.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'page not complete'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="front">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_inner-v-centered sHero">
+      <div class="section__bg-wrap">
+        <div class="section__bg section__bg_mobile" style="background-image: url('../dist/images/tmp/section-bg-img-1-mobile.jpg')"></div>
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-1.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_a">
+        <div class="bContainer">
+
+          <div class="bTitle bTitle_b">
+            <h1>Page Not Complete</h1>
+          </div>
+
+      </div>
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
