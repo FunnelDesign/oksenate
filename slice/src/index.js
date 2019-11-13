ce49245f8444654fea746ad24c095130b389e46c
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-13 18:59:54 +0300

    TEST

--- slice/src/index.js
+++ slice/src/index.js
@@ -5,33 +5,29 @@ import Observer from "./components/Observer/Observer";
 
 const $ = jQuery;
 
-// (function () {
-
-	if (window.Drupal?.behaviors) {
-		console.log('drupal init Test 3');
-
-		Drupal.behaviors.projectName = {
-			attach: function (context, settings) {
-				init();
-			},
-			completedCallback: function () {
-				// Do nothing. But it's here in case other modules/themes want to override it.
-			}
-		}
-	} else {
-		document.addEventListener('DOMContentLoaded', () => {
-			init();
-		});
-	}
-
-	new Observer();
-
-	function init() {
-		console.log(window.Drupal?.behaviors, 'new window.Drupal?.behaviors');
-		new bEvent();
-	}
-// })();
-
 window.addEventListener('load', () => {
 	$('body').addClass('loaded');
 });
+
+if (window.Drupal?.behaviors) {
+	console.log('drupal init Test 66');
+
+	Drupal.behaviors.projectName = {
+		attach: (context, settings) => {
+			init();
+		},
+		completedCallback: () => { /*Do nothing. But it's here in case other modules/themes want to override it.*/}
+	}
+} else {
+	document.addEventListener('DOMContentLoaded', () => {
+		init();
+	});
+}
+
+new Observer();
+
+function init() {
+	console.log(window.Drupal?.behaviors, 'new window.Drupal?.behaviors');
+	new bEvent();
+}
+
