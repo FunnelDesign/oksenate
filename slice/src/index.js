1ece0c6c464c6dac997e104d42ff5f6c5b1bf1cc
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-13 17:45:55 +0300

    TEST

--- slice/src/index.js
+++ slice/src/index.js
@@ -34,6 +34,32 @@ window.addEventListener('load', () => {
 	$('body').addClass('loaded');
 });
 
+(function ($) {
+	if (typeof Drupal != 'undefined') {
+
+		console.log('drupal init TEST2222222222');
+		Drupal.behaviors.projectName = {
+			attach: function (context, settings) {
+				initTmpQQQQQ();
+			},
+
+			completedCallback: function () {
+				// Do nothing. But it's here in case other modules/themes want to override it.
+			}
+		}
+	}
+
+	$(function () {
+		if (typeof Drupal == 'undefined') {
+			initTmpQQQQQ();
+		}
+	});
+
+	function initTmpQQQQQ() {
+		console.log(window.Drupal?.behaviors, 'QQQQQQQQQQQQQQQQ');
+	}
+
+})(jQuery);
 
 (function ($) {
 	if (typeof Drupal != 'undefined') {
