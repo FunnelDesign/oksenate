9c222d443abb914d798da6afa86c5b765c928c63
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-13 17:37:18 +0300

    19-1642-Senator-FindMySenator

--- slice/src/index.js
+++ slice/src/index.js
@@ -1,5 +1,32 @@
 import './styles'
 
+(function ($) {
+	if (typeof Drupal != 'undefined') {
+
+		console.log('drupal init TEST');
+		Drupal.behaviors.projectName = {
+			attach: function (context, settings) {
+				initTEST();
+			},
+
+			completedCallback: function () {
+				// Do nothing. But it's here in case other modules/themes want to override it.
+			}
+		}
+	}
+
+	$(function () {
+		if (typeof Drupal == 'undefined') {
+			initTEST();
+		}
+	});
+
+	function initTEST() {
+		console.log(window.Drupal?.behaviors, 'TESTSSTTST');
+	}
+
+})(jQuery);
+
 import bEvent from './components/bEvent/bEvent'
 import Observer from "./components/Observer/Observer";
 const $ = jQuery;
@@ -13,7 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
 			attach: function (context, settings) {
 				init();
 			},
-			completedCallback: function () {/*Do nothing. But it's here in case other modules/themes want to override it.*/	}
+			completedCallback: function () {
+				// Do nothing. But it's here in case other modules/themes want to override it.
+			}
 		}
 	} else {
 		init();
