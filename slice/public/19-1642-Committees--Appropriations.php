1baf5e765cfc15afcf99ed42e9cb069e4b270f1a
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-16 15:42:42 +0300

    Committees 19-1642-Committees--Appropriations

--- slice/public/19-1642-Committees--Appropriations.php
+++ slice/public/19-1642-Committees--Appropriations.php
@@ -43,8 +43,184 @@
       </div>
     </section>
 
+    <section class="section section_ind_f section_bg-color_b">
+
+      <div class="bContainer">
+        <div class="senators senators_col senators_dark">
+
+          <div class="senators__items">
+                  <span class="senators__item">
+                  <span class="senators__imgWrap">
+                      <span class="senators__img" style="background-image: url('../dist/images/tmp/senators-img-3.jpg')"></span>
+                    </span>
+
+                    <span class="senators__name">
+                     Sen. Casey Murdock
+                    </span>
+                    <span class="senators__position">
+                      Chairman
+                    </span>
+
+                  </span>
+
+            <a href="#" class="senators__item">
+                    <span class="senators__imgWrap">
+                      <span class="senators__img" style="background-image: url('../dist/images/tmp/senators-img-4.jpg')"></span>
+                    </span>
+
+              <span class="senators__name">
+                     Sen. Roland Pederson
+                    </span>
+              <span class="senators__position">
+                     Vice Chairman
+                    </span>
+
+            </a>
+
+          </div>
+
+          <div class="senators__all">
+
+            <h2>Appropriations <br>
+              Members</h2>
+
+            <div class="senators__btnWrap">
+              <a href="#" class="btn">see all members</a>
+            </div>
 
 
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="pageIn__ico">
+          <img src="../dist/images/titleIco/title-ico-1.png" srcset="../dist/images/titleIco/title-ico-1-2x.png 2x" alt="">
+        </div>
+        <h2>Subcommittee Events</h2>
+        
+        <div class="bTiles bTiles_ev">
+
+          <div class="bTiles__items">
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+                Thur. Nov. 7, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </span>
+              <span class="bTiles__sub">
+                Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn bTiles__btn_live">
+                  <span><span>•</span> live now</span> room 511-a
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+                Tues. Nov. 12, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </span>
+              <span class="bTiles__sub">
+                Fourth Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn">
+                  <span><span>•</span></span> room 511-a
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </span>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn">
+                  <span><span>•</span></span> room 535
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+              Thur. Nov. 17, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </span>
+              <span class="bTiles__sub">
+              Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn">
+                  <span><span>•</span></span> room 511-a
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+              Tues. Nov. 19, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+               Public Safety and Judiciary Subcommittee Budget Hearings
+
+              </span>
+              <span class="bTiles__sub">
+              Fourth Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn">
+                  <span><span>•</span></span> room 511-a
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+              <span class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </span>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="bTiles__btn">
+                  <span><span>•</span></span> room 535
+                </span>
+              </span>
+
+            </a>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
 
 
   </div>
