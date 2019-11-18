86dea3945f0c94b21d6098d8dd4c37b201c1a175
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-18 14:53:15 +0300

    #111904 Develop Calendar Joint A& B page

---
+++ slice/public/19-1642-Calendar-JointAB.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Calendar-JointAB'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Calendar</a>
+            <span>Joint Appropriations & Budget</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-10.png" srcset="../dist/images/titleIco/title-ico-10-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Joint Appropriations & Budget</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_f section_bg-color_c">
+
+      <div class="bContainer">
+
+        <div class="bTitle bTitle_wSub_a bTitle_gap_b">
+          <h1>Joint Calendar for Appropriations & Budget</h1>
+          <div class="bTitle__sub bTitle__sub_a">
+            Joint Rule 4.12
+          </div>
+        </div>
+
+        <div class="bTb bTb_a">
+
+          <div class="bTb__tHead">
+            <div class="bTb__tr">
+
+              <div class="bTb__th">
+                bill #
+              </div>
+
+              <div class="bTb__th">
+                title
+              </div>
+
+              <div class="bTb__th">
+                report date
+              </div>
+
+              <div class="bTb__th">
+
+              </div>
+
+            </div>
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1072</strong>
+              </div>
+
+              <div class="bTb__td">
+                State Department of Health funding; reducing amount of appropriation for Fiscal Year 2019; making appropriations to the Special Cash Fund of the State Treasury; emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>05/10/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1076</strong>
+              </div>
+
+              <div class="bTb__td">
+                Public finance; specifying certain duty. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/14/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1078</strong>
+              </div>
+
+              <div class="bTb__td">
+                Revenue and taxation; modifying required documents. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/20/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1072</strong>
+              </div>
+
+              <div class="bTb__td">
+                State Department of Health funding; reducing amount of appropriation for Fiscal Year 2019; making appropriations to the Special Cash Fund of the State Treasury; emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>05/10/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1076</strong>
+              </div>
+
+              <div class="bTb__td">
+                Public finance; specifying certain duty. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/14/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1078</strong>
+              </div>
+
+              <div class="bTb__td">
+                Revenue and taxation; modifying required documents. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/20/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1072</strong>
+              </div>
+
+              <div class="bTb__td">
+                State Department of Health funding; reducing amount of appropriation for Fiscal Year 2019; making appropriations to the Special Cash Fund of the State Treasury; emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>05/10/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1076</strong>
+              </div>
+
+              <div class="bTb__td">
+                Public finance; specifying certain duty. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/14/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1078</strong>
+              </div>
+
+              <div class="bTb__td">
+                Revenue and taxation; modifying required documents. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/20/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1072</strong>
+              </div>
+
+              <div class="bTb__td">
+                State Department of Health funding; reducing amount of appropriation for Fiscal Year 2019; making appropriations to the Special Cash Fund of the State Treasury; emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>05/10/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1076</strong>
+              </div>
+
+              <div class="bTb__td">
+                Public finance; specifying certain duty. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/14/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>SB 1078</strong>
+              </div>
+
+              <div class="bTb__td">
+                Revenue and taxation; modifying required documents. Emergency.
+              </div>
+
+              <div class="bTb__td">
+                <strong>5/20/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="btn btn_b btn_b_sA">download</a>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
