684be40aa8aa70a3b66aaddf8d67d305a803ff4b
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-18 14:21:09 +0300

    #111906 Develop calendar landing page

---
+++ slice/public/19-1642-Calendar.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Calendar'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-6.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-8.png" srcset="../dist/images/titleIco/title-ico-8-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Calendar</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section">
+      <div class="bTiles bTiles_nav">
+        <div class="bTiles__items">
+
+          <div class="bTiles__item">
+            <span class="bTiles__ico">
+              <img src="../dist/images/tmp/tiles-ico-img-4.png"
+                   srcset="../dist/images/tmp/tiles-ico-img-4-2x.png 2x"
+                   alt="">
+            </span>
+            <span class="bTiles__btnWrap">
+                <a href="#" class="btn">state journals</a>
+            </span>
+          </div>
+
+          <div class="bTiles__item">
+            <span class="bTiles__ico">
+               <img src="../dist/images/tmp/tiles-ico-img-5.png"
+                    srcset="../dist/images/tmp/tiles-ico-img-5-2x.png 2x"
+                    alt="">
+            </span>
+            <span class="bTiles__btnWrap">
+                <a href="#" class="btn">joint a&b</a>
+            </span>
+          </div>
+
+          <div class="bTiles__item">
+            <span class="bTiles__ico">
+               <img src="../dist/images/tmp/tiles-ico-img-6.png"
+                    srcset="../dist/images/tmp/tiles-ico-img-6-2x.png 2x"
+                    alt="">
+            </span>
+            <span class="bTiles__btnWrap">
+                <a href="#" class="btn">joint conference</a>
+            </span>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="section section_ind_md section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="pageIn__ico">
+          <img src="../dist/images/titleIco/title-ico-9.png" srcset="../dist/images/titleIco/title-ico-9-2x.png 2x"
+               alt="">
+        </div>
+        <h2>Upcoming Deadlines</h2>
+        <div class="bDateList">
+
+          <div class="bDateList__items">
+
+            <div class="bDateList__item">
+              
+              <div class="bDateList__date">
+                <span>dec</span>
+                13
+              </div>
+              <div class="bDateList__desc">
+                Bill Request Deadline
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>jan</span>
+                16
+              </div>
+              <div class="bDateList__desc">
+                Bill Introduction Deadline, 4:00pm
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>feb</span>
+                3
+              </div>
+              <div class="bDateList__desc">
+                Session Begins, 12:00pm
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>feb</span>
+                27
+              </div>
+              <div class="bDateList__desc">
+                Senate Measures from Senate Committees Deadline
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>mar</span>
+                12
+              </div>
+              <div class="bDateList__desc">
+                3rd Reading in House of Origin Deadline
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>apr</span>
+                9
+              </div>
+              <div class="bDateList__desc">
+                House Measures from Senate Committees Deadline
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>apr</span>
+                23
+              </div>
+              <div class="bDateList__desc">
+                3rd Reading in Opposite House Deadline
+              </div>
+
+            </div>
+
+            <div class="bDateList__item">
+
+              <div class="bDateList__date">
+                <span>may</span>
+                29
+              </div>
+              <div class="bDateList__desc">
+                Sine Die Adjournment, 5:00pm
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </section>
+
+    <section class="section section_ind_md section_bg-color_b">
+
+      <div class="bContainer">
+        <div class="pageIn__ico">
+          <img src="../dist/images/titleIco/title-ico-3.png" srcset="../dist/images/titleIco/title-ico-3-2x.png 2x" alt="">
+        </div>
+        <h2>Latest State Journals</h2>
+
+        <div class="bTiles bTiles_rel bTiles_gap_a">
+
+          <div class="bTiles__items">
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__title">
+               May 23, 2019
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                  view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__title">
+              May 22, 2019
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                  view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__title">
+              May 21, 2019
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                  view now
+                </span>
+              </span>
+
+            </a>
+          </div>
+
+          <div class="bTiles__btnAllWrap">
+            <a href="#" class="btn">see all</a>
+          </div>
+
+        </div>
+
+        <div class="pageIn__ico">
+          <img src="../dist/images/tmp/tiles-ico-img-5.png" srcset="../dist/images/tmp/tiles-ico-img-5-2x.png 2x" alt="">
+        </div>
+        <h2>Latest Joint Appropriations & Budget Bills</h2>
+
+        <div class="bTb bTb_a">
+          <div class="bTb__tr">
+
+            <div class="bTb__td">
+              <strong>SB 1072</strong>
+            </div>
+
+            <div class="bTb__td">
+              State Department of Health funding; reducing amount of appropriation for Fiscal Year 2019; making appropriations to the Special Cash Fund of the State Treasury; emergency.
+            </div>
+
+            <div class="bTb__td">
+              <strong>05/10/2019</strong>
+            </div>
+
+            <div class="bTb__td">
+              <a href="#" class="btn btn_b btn_b_sA">download</a>
+            </div>
+
+          </div>
+
+          <div class="bTb__tr">
+
+            <div class="bTb__td">
+              <strong>SB 1076</strong>
+            </div>
+
+            <div class="bTb__td">
+              Public finance; specifying certain duty. Emergency.
+            </div>
+
+            <div class="bTb__td">
+              <strong>5/14/2019</strong>
+            </div>
+
+            <div class="bTb__td">
+              <a href="#" class="btn btn_b btn_b_sA">download</a>
+            </div>
+
+          </div>
+
+          <div class="bTb__tr">
+
+            <div class="bTb__td">
+              <strong>SB 1078</strong>
+            </div>
+
+            <div class="bTb__td">
+              Revenue and taxation; modifying required documents. Emergency.
+            </div>
+
+            <div class="bTb__td">
+              <strong>5/20/2019</strong>
+            </div>
+
+            <div class="bTb__td">
+              <a href="#" class="btn btn_b btn_b_sA">download</a>
+            </div>
+
+          </div>
+        </div>
+        <div class="pageIn__btnAllWrap">
+          <a href="#" class="btn">see all</a>
+        </div>
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
