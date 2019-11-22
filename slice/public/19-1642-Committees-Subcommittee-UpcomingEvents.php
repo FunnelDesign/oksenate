0b82cdb89662485c159459829d68b759a5d0ca1c
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-22 13:01:12 +0300

    19-1642-Committees-Subcommittee-UpcomingEvents

---
+++ slice/public/19-1642-Committees-Subcommittee-UpcomingEvents.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Committees-Subcommittee-UpcomingEvents'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_j section_bg-color_c">
+
+
+      <div class="section__sNav section__sNav_a">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_b link_back">back</a>
+          <div class="breadcrumb breadcrumb_a">
+            <a href="#">Committees</a>
+            <a href="#">Appropriations   </a>
+            <a href="#">Education   </a>
+            <span>Upcoming Events</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="bContainer">
+        <div class="pageIn__ico">
+          <img src="../dist/images/titleIco/title-ico-1.png" srcset="../dist/images/titleIco/title-ico-1-2x.png 2x" alt="">
+        </div>
+        <h2>Upcoming Education Events</h2>
+        
+        <div class="bTiles bTiles_ev bTiles_gap_a">
+
+          <div class="bTiles__items">
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+                Thur. Nov. 7, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+                Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b btn_b_live">
+                  <span><span>•</span> live now</span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+              Thur. Nov. 17, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+              Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+             <a href="#" class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </a>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                 <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 535
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+                Thur. Nov. 7, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+                Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b btn_b_live">
+                  <span><span>•</span> live now</span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+              Thur. Nov. 17, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+              Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+             <a href="#" class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </a>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                 <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 535
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+                Thur. Nov. 7, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+                Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b btn_b_live">
+                  <span><span>•</span> live now</span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+              Thur. Nov. 17, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+              Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+             <a href="#" class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </a>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                 <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 535
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+                Thur. Nov. 7, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+                Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b btn_b_live">
+                  <span><span>•</span> live now</span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+              Thur. Nov. 17, 2019 |  9:00am
+              </span>
+              <a href="#" class="bTiles__title">
+                Public Safety and Judiciary Subcommittee Budget Hearings
+              </a>
+              <span class="bTiles__sub">
+              Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 511-a
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__date">
+               Fri. Nov. 15, 2019 |  9:00am
+              </span>
+             <a href="#" class="bTiles__title">
+                Oversight Committee for the Legislative Office of Fiscal Transparency (LOFT)
+              </a>
+              <span class="bTiles__sub">
+               Third Meeting
+              </span>
+              <span class="bTiles__btnWrap">
+                 <a href="#" class="bTiles__btn btn btn_b">
+                  <span><span>•</span></span> room 535
+                </a>
+              </span>
+
+            </span>
+
+          </div>
+        </div>
+
+
+        <div class="pageIn__ico">
+          <img src="../dist/images/titleIco/title-ico-4.png" srcset="../dist/images/titleIco/title-ico-4-2x.png 2x" alt="">
+        </div>
+        <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+          <h1>Sign Up for Notifications</h1>
+        </div>
+
+        <div class="form fSignUp">
+          <div class="fSignUp__desc">
+            <p>Enter your name and email below to receive updates about <br>
+              the Education subcommittee.</p>
+          </div>
+          <div class="fSignUp__items">
+            <div class="form-item">
+              <input type="text" class="form-text" placeholder="First Name"/>
+            </div>
+            <div class="form-item">
+              <input type="text" class="form-text" placeholder="Last Name"/>
+            </div>
+            <div class="form-item">
+              <input type="text" class="form-text" placeholder="Email Address"/>
+            </div>
+          </div>
+          <div class="form-actions">
+            <input type="submit" class="form-submit" value="submit">
+            <div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
