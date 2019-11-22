82f176247724b5101a8afa4b5b3416e903e3b757
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-22 15:54:41 +0300

    19-1642-Senator-IndividualPage4

---
+++ slice/public/19-1642-PressReleases-Individual.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Senator-IndividualPage4'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_d section_bg-color_c">
+
+      <div class="bContainer">
+        <a href="#" class="btn btn_a section__btnBack">
+          back to press releases
+        </a>
+        
+        <div class="bPost">
+          <div class="bPost__header">
+            <div class="bPost__date">
+              February 28, 2019 |  9:00am
+            </div>
+            <div class="bPost__title">
+              <h1>Measure to build aerospace training facility heads to full Senate vote</h1>
+            </div>
+          </div>
+          <div class="bPost__body">
+            <div class="bPost__imgWrap">
+              <div class="bPost__img">
+                <img src="../dist/images/tmp/img-tmp-post-1.jpg" alt="">
+                <span class="bPost__imgCap">
+                  Sen. Adam Pugh
+                </span>
+              </div>
+            </div>
+            <div class="bPost__text">
+              <p>A measure to build a statewide aerospace training facility cleared both the Senate Education and the Appropriation and Budget committees this week and is now headed to the Senate floor.</p>
+              <p>Senate Bill 432, authored by Sen. Adam Pugh, R-Edmond, authorizes the Oklahoma Department of Career and Technology Education to acquire or lease property for the purpose of creating the statewide aerospace training facility. Under the measure, the State Board would establish admission standards and the curriculum requirements.</p>
+              <p>Pugh said he filed the legislation after officials at the state’s air force bases voiced a growing need for job candidates with specific skill sets in Oklahoma. Aerospace and aviation is the state’s second largest industry and still growing rapidly, with an annual economic impact of $44 billion and providing 205,000 direct and indirect jobs with an average salary of $73,300.</p>
+              <p>“Creating a state aerospace training facility is a logical and necessary step in continuing to support the rapid expansion of the industry in Oklahoma,” said Pugh. “The increasing visibility of aviation and aerospace in Oklahoma has not gone unnoticed by military officials and aerospace executives, as industry leaders continue to push for broadened job training opportunities to fill hundreds of available jobs in the aviation industry across the state.”</p>
+              <p>Pugh noted the statewide program would have no district boundaries, which would be a different approach from CareerTech programming, allowing students from anywhere in the state to access the training center. Pugh says ensuring the industry’s viability is critical to the continuing effort to diversify Oklahoma’s economy, and the creation of an aeronautics training center will continue to fuel that exponential growth.</p>
+              <p>“This would be unique from the perspective that students from all four corners of the state and everywhere in between would have the same learning opportunities, whether through mobile distance learning or in-residence training. Hands-on materials could be packed up and transported around the state as needed to facilitate those students engaged in distance learning,” Pugh said. “Building this center allows the state to maximize the competitive advantages that already exist in Oklahoma’s aerospace industry. Cultivating the skill sets needed in the aerospace industry will help to drive better jobs, stronger economic growth and higher tax revenues for Oklahoma.”</p>
+              <p>Pugh said in addition to a line item appropriation in the state budget for the training center, he is seeking other sources of funding, including grants, partnerships with Tinker Air Force Base, and private sector partners. He also is looking to source donated materials like aircraft parts and components that would offer realistic, hands-on training opportunities for students.</p>
+              <p>SB 432 now moves to the Senate floor for consideration.</p>
+              <p>For more information, contact: <br>
+                Sen. Pugh: <a href="tel:+(405) 521-5622">(405) 521-5622</a></p>
+            </div>
+
+          </div>
+
+          <div class="bSoc bSoc_min">
+
+            <div class="bSoc__items">
+              <div class="bSoc__item">
+                <a target="_blank" href="#">
+                  <img src="../dist/images/soc/soc-facebook.png"
+                       srcset="../dist/images/soc/soc-facebook-2x.png 2x"
+                       alt="">
+                </a>
+              </div>
+              <div class="bSoc__item">
+                <a target="_blank" href="#">
+                  <img src="../dist/images/soc/soc-twitter.png"
+                       srcset="../dist/images/soc/soc-twitter-2x.png 2x"
+                       alt="">
+                </a>
+              </div>
+              <div class="bSoc__item">
+                <a target="_blank" href="#">
+                  <img src="../dist/images/soc/soc-mail.png"
+                       srcset="../dist/images/soc/soc-mail-2x.png 2x"
+                       alt="">
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+
+</div>
+</body>
+</html>
