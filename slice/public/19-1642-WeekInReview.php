c743e6d8ac2aeb43ad9b3afb3c88c694697f70ea
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-25 13:49:37 +0300

    #112057 19-1642-WeekInReview

---
+++ slice/public/19-1642-WeekInReview.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-WeekInReview'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-10.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-14.png" srcset="../dist/images/titleIco/title-ico-14-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+            <h1>Week In Review</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="bPostList__sortBy">
+          Showing: All
+        </div>
+
+        <div class="pageIn__pCols">
+          <aside class="bSidebar pageIn__bSidebar">
+            <?php include 'tpl/blocks/bSort.inc'; ?>
+          </aside>
+
+          <div class="pageIn__main">
+
+            <div class="bPostList">
+
+              <div class="bPostList__items">
+
+                <div class="bPostList__item">
+                  <div class="bPostList__header">
+
+                    <div class="bPostList__date">
+                      March 5, 2019
+                    </div>
+
+                    <a href="#" class="bPostList__title">
+                      Monday, March 5, to Wed., March 8, 2018
+                    </a>
+
+                  </div>
+
+
+                  <div class="bPostList__text">
+                    <p>
+                      • The Senate met Monday, passing the following bills: <br>
+                      -SB 1043, Sen. Paul Rosino and Rep. Lewis Moore, modifies
+                      the fee county clerks may charge for records. The bill eliminates
+                      the term "reasonable fee." The bill provides county clerks in
+                      counties with  ... <a href="#" class="bPostList__readMore">read more.</a>
+
+                    </p>
+                  </div>
+
+                  <?php include 'tpl/blocks/bSoc.inc'; ?>
+                </div>
+
+                <div class="bPostList__item">
+                  <div class="bPostList__header">
+
+                    <div class="bPostList__date">
+                      February 26, 2019
+                    </div>
+
+                    <a href="#" class="bPostList__title">
+                      Monday, Feb. 26, to Wed., March 1, 2018
+                    </a>
+
+                  </div>
+
+
+                  <div class="bPostList__text">
+                    <p>
+                      • The Senate met Monday, passing the following bills: <br>
+                      -SB 1043, Sen. Paul Rosino and Rep. Lewis Moore, modifies
+                      the fee county clerks may charge for records. The bill eliminates
+                      the term "reasonable fee." The bill provides county clerks in
+                      counties with  ... <a href="#" class="bPostList__readMore">read more.</a>
+
+                    </p>
+                  </div>
+
+                  <?php include 'tpl/blocks/bSoc.inc'; ?>
+                </div>
+
+                <div class="bPostList__item">
+                  <div class="bPostList__header">
+
+                    <div class="bPostList__date">
+                      February 19, 2019
+                    </div>
+
+                    <a href="#" class="bPostList__title">
+                      Monday, Feb. 19, to Wed., Feb. 22, 2018
+                    </a>
+
+                  </div>
+
+
+                  <div class="bPostList__text">
+                    <p>
+                      • The Senate met Monday, passing the following bills: <br>
+                      -SB 1043, Sen. Paul Rosino and Rep. Lewis Moore, modifies
+                      the fee county clerks may charge for records. The bill eliminates
+                      the term "reasonable fee." The bill provides county clerks in
+                      counties with  ... <a href="#" class="bPostList__readMore">read more.</a>
+
+                    </p>
+                  </div>
+
+                  <?php include 'tpl/blocks/bSoc.inc'; ?>
+                </div>
+
+                <div class="bPostList__item">
+                  <div class="bPostList__header">
+
+                    <div class="bPostList__date">
+                      February 12, 2019
+                    </div>
+
+                    <a href="#" class="bPostList__title">
+                      Monday, Feb. 12, to Wed., Feb. 15, 2018
+                    </a>
+
+                  </div>
+
+
+                  <div class="bPostList__text">
+                    <p>
+                      • The Senate met Monday, passing the following bills: <br>
+                      -SB 1043, Sen. Paul Rosino and Rep. Lewis Moore, modifies
+                      the fee county clerks may charge for records. The bill eliminates
+                      the term "reasonable fee." The bill provides county clerks in
+                      counties with  ... <a href="#" class="bPostList__readMore">read more.</a>
+
+                    </p>
+                  </div>
+
+                  <?php include 'tpl/blocks/bSoc.inc'; ?>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="bPostList__pager">
+              <div class="bPostList__pagerTitle">
+                Page:
+              </div>
+              <nav class="pager">
+                <ul class="pager__items js-pager__items">
+                  <li class="pager__item pager__item--previous">
+                    <a href="#">
+                      <span></span>
+                    </a>
+                  </li>
+                  <li class="pager__item is-active">
+                    <a href="#">
+                      1
+                    </a>
+                  </li>
+                  <li class="pager__item">
+                    <a href="#">
+                      2
+                    </a>
+                  </li>
+                  <li class="pager__item">
+                    <a href="#">
+                      3
+                    </a>
+                  </li>
+                  <li class="pager__item">
+                    <a href="#">
+                      4
+                    </a>
+                  </li>
+                  <li class="pager__item pager__item--next">
+                    <a href="#">
+                      <span></span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+
+</div>
+</body>
+</html>
