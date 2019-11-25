48a767cdb429ee1079f5ad17893400fb13374859
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-25 17:19:08 +0300

    #112048 Develop Publications > Appropriation Reports page

---
+++ slice/public/19-1642-Publications-AppropriationsReports.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-AppropriationsReports'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Publications</a>
+            <span>Appropriations Reports</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-16.png" srcset="../dist/images/titleIco/title-ico-16-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Appropriations Reports</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <div class="bTitle bTitle_gap_c">
+          <div class="bTitle__sub bTitle__sub_b">
+            <p>Select a brief below to view the PDF.</p>
+          </div>
+        </div>
+
+        <div class="bColumns bColumns_2 bWrap bWrap_size_b">
+          <div class="bListLinks bListLinks_btnArr">
+
+            <a href="#" class="bListLinks__link">View FY’18-19 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’17 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’16 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’15 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’14 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’13 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’12 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’11 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’10 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’9 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+            <a href="#" class="bListLinks__link">View FY’8 Appropriations Report PDF</a>
+
+          </div>
+
+        </div>
+
+
+
+
+
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
