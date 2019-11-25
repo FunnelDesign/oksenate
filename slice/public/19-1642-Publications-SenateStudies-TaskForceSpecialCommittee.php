afa130d2c8b00fb1ce94d56b8b666dcf691e5506
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-25 19:52:57 +0300

    #112054 Develop Publications > Senate Studies > Task Force and Special Committes

---
+++ slice/public/19-1642-Publications-SenateStudies-TaskForceSpecialCommittee.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-SenateStudies-TaskForceSpecialCommittee'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_d section_bg-color_c">
+
+      <div class="bContainer">
+        <a href="#" class="btn btn_a section__btnBack">
+          back to senate studies
+        </a>
+        
+        <div class="bPost bPost_b">
+          <div class="bPost__header">
+            <div class="bPost__title">
+              <h1>legislative studies and special committees created by legislation</h1>
+              <div class="bPost__sub">
+                Revised: August 29, 2011
+              </div>
+            </div>
+          </div>
+          <div class="bPost__body">
+
+            <div class="bPost__text">
+              <h5>African-American Centennial Plaza Design Committee</h5>
+
+              <div class="bTable bTable_a bTable_s_a bTable_gap_a">
+                <table>
+                  <tbody>
+                  <tr>
+                    <td><strong>Authority</strong> </td>
+                    <td><a href="#">SB 1919</a>(2006)</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Principal Authors
+                      </strong> </td>
+                    <td>Sen. Constance E. Johnson <br>
+                      Rep. Jabar Shumate</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Appointees</strong> </td>
+                    <td>Senate <br>
+                      Sen. Constance E. Johnson</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Staffed By</strong> </td>
+                    <td>
+                      Oklahoma Historical Society <br>
+                      Senate and House </td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Expiration Date</strong> </td>
+                    <td>
+                      Not Specified
+                  </tr>
+
+                  </tbody>
+                </table>
+              </div>
+
+              <h5>Business and Professional License Facilitation Task Force</h5>
+
+              <div class="bTable bTable_a bTable_s_a">
+                <table>
+                  <tbody>
+                  <tr>
+                    <td><strong>Authority</strong> </td>
+                    <td><a href="#">SB 1919</a>(2006)</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Principal Authors
+                      </strong> </td>
+                    <td>Sen. Constance E. Johnson <br>
+                      Rep. Jabar Shumate</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Appointees</strong> </td>
+                    <td>Senate <br>
+                      Sen. Constance E. Johnson</td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Staffed By</strong> </td>
+                    <td>
+                      Oklahoma Historical Society <br>
+                      Senate and House </td>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Expiration Date</strong> </td>
+                    <td>
+                      Not Specified
+                  </tr>
+
+                  </tbody>
+                </table>
+              </div>
+
+
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+
+</div>
+</body>
+</html>
