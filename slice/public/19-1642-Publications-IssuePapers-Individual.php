5259239a5406d5faeb0ae69f2dcf603d557fabff
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-25 19:21:18 +0300

    #112049 Develop Publications > Individual Issue pager page

---
+++ slice/public/19-1642-Publications-IssuePapers-Individual.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-IssuePapers-Individual'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_d section_bg-color_c">
+
+      <div class="bContainer">
+        <a href="#" class="btn btn_a section__btnBack">
+          back to issue papers
+        </a>
+        
+        <div class="bPost bPost_a">
+          <div class="bPost__header">
+            <div class="bPost__title">
+              <h1>indian land tax credit issues:</h1>
+              <h2>Federal Property and Employment Tax Benefits for Indian Lands <br>
+                by  <br>
+                Senate Staff  <br>
+                October, 1997</h2>
+            </div>
+          </div>
+          <div class="bPost__body">
+
+            <div class="bPost__text">
+             <p>In the Budget Reconciliation Act of 1993, Congress provided income tax incentives to businesses located on Indian reservations. The way the term "Indian reservations" was used in the bill caused confusion as to Congressional intent. To resolve this issue, Congress further defined "reservations" and portions of Oklahoma that qualify as reservations in the Taxpayer Relief Act of 1997. With this latest action, Congress has made certain its intent for application of these tax incentives in Oklahoma.</p>
+              <ul>
+                <li>The Omnibus Budget Reconciliation Act of 1993 added two federal income tax incentives designed to stimulate economic development on Indian reservations throughout the United States. <br> <br>
+
+                  - The accelerated depreciation for property on Indian reservations allowed businesses to depreciate
+                  their capital expenses more rapidly than other properties, which could reduce a business's tax
+                  liability earlier and in turn improve the present value of its after-tax income. <br> <br>
+
+                  - The tax credit for Indian employees allowed businesses to use a portion of the wages paid and
+                  health insurance benefits provided to Indian employees to directly reduce their tax liability and in
+                  turn increase after-tax income.
+                </li>
+                
+                <li>Due to Oklahoma's unique history, it has a very high Native American population but no current "Indian reservations". The Taxpayer Relief Act of 1997 clarifies the definition of the term "Indian reservation" in Oklahoma as "lands which are within the jurisdictional area of an Oklahoma Indian tribe (as determined by the Secretary of the Interior)".</li>
+                
+                <li>To further clarify Congressional intent, Congressman Wes Watkins constructed a "bright-line test" in which the eligible lands are determined to be "lands within boundaries of the last treaties with the Oklahoma tribes".</li>
+                <li>Currently, approximately two-thirds of Oklahoma qualifies for the depreciation and employment tax credit incentives. The areas that do not qualify are historically identified as No Man's Land, Old Greer County, the Unassigned Lands, and the Cherokee Outlet.</li>
+
+              </ul>
+              <p>&nbsp;</p>
+              <p><img src="../dist/images/tmp/tmp-img-1-content.jpg " alt=""></p>
+              <p>&nbsp;</p>
+              <p>The passage of the Taxpayer Relief Act of 1997 on August 5, clarified the term "Indian reservation" as it pertains to Oklahoma as "lands which are within the jurisdictional area of an Oklahoma Indian tribe". For purposes of Congressional intent, the House Ways and Means Committee defines an Indian reservations in Oklahoma as "lands within boundaries of the last treaties with the Oklahoma tribes." This definition satisfied Committee Chairman Bill Archer in that it narrowed the scope of the qualifying lands by eliminating the Unassigned Lands.</p>
+              <p>The Internal Revenue Code of 1986 (as amended) now provides for additional accelerated depreciation of property placed on an Indian reservation (as defined above). To be eligible, the property must:</p>
+              <p>be used by the taxpayer predominantly in the active conduct of a trade or business within an Indian reservation on a regular basis.</p>
+              <p>not be used or located outside the Indian reservation on a regular basis.</p>
+              <p>not be used in conducting or housing class I, II or III gaming as defined in the Indian Regulatory Act.</p>
+              <p>not be residential rental property.</p>
+              <p>not be owned by a person who is required to use the "alternative depreciation system".</p>
+              <h4>background</h4>
+              
+              <p>In 1992 Senator John McCain, R-Arizona, drafted a tax bill with the assistance of the Navajo Nation to make it more attractive for businesses to hire Native Americans and to invest in reservation land. This bill was ultimately vetoed by President Bush.</p>
+              <p>The 1993 budget-reconciliation bill included "empowerment zones" aimed at low-income inner cities and poor rural areas. McCain fought to make Indians eligible for the program as well arguing that Indians are among the poorest Americans. Oklahoma Indians were often excluded from these types of assistance packages because they are commonly aimed at Indian "reservations". Oklahoma has no official reservations, because land that was once reservation land has since either been allotted for settlement or given to individuals or tribes. Then Oklahoma Senator David Boren insisted that Oklahoma Indians be included in the tax incentives.</p>
+              <p>The Omnibus Reconciliation Act of 1993 was signed into law on August 10, 1993. It contained two provisions to stimulate economic development on Indian reservations by providing assistance to companies investing in and conducting business on such lands between the years 1994 and 2003. These tax incentives were similar to those targeted at other areas of economic depression, known as empowerment zones. First, the Act authorized Accelerated Depreciation Values for Property on Indian Reservations and second, it outlined the Indian Employment Credit.</p>
+              <p>The accelerated depreciation for property on Indian reservations allowed businesses to depreciate their capital expenses more rapidly than other properties, which could reduce a business's tax liability earlier and in turn improve the present value of its after-tax income.</p>
+
+
+            </div>
+
+          </div>
+
+          <?php include 'tpl/blocks/bSoc.inc'; ?>
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+
+</div>
+</body>
+</html>
