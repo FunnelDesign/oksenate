4d4139fbdfe4f8c492719ae7a4d450ccb025d8e2
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-25 17:41:20 +0300

    #112056 Develop Publications > Overview of State Issues

---
+++ slice/public/19-1642-Publications-StateIssueOverview.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-LegislativeBriefs'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Publications</a>
+            <span>Legislative Briefs</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-17.png" srcset="../dist/images/titleIco/title-ico-17-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Legislative Briefs</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <div class="bTitle bTitle_gap_c">
+          <div class="bTitle__sub bTitle__sub_b">
+            <p>Select a brief below to view the PDF.</p>
+          </div>
+        </div>
+
+        <div class="bColumns bColumns_2 bWrap bWrap_size_b">
+          <div class="bListLinks bListLinks_btnArr">
+
+            <a href="#" class="bListLinks__link">View 2018 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2017 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2016 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2015 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2014 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2013 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2012 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2011 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2010 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2009 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2008 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2007 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2006 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2005 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2004 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2003 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2002 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 2001 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 1999 Legislative Briefs PDF</a>
+            <a href="#" class="bListLinks__link">View 1998 Legislative Briefs PDF</a>
+
+          </div>
+
+        </div>
+
+
+
+
+
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
