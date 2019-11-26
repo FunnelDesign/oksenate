f556c7991567b60e7a74a84ab6f4c02a0e1f2884
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-26 16:00:03 +0300

    #112052 Develop Publications > Senate Studies > Meeting Notice page

---
+++ slice/public/19-1642-Publications-SenateStudies-MeetingNotice.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-SenateStudies-MeetingNotice'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Publications</a>
+            <a href="#">Senate Studies </a>
+            <span>Meeting Notice</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-12.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Meeting Notice</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+       <h2>Committee on Education</h2>
+       <div class="pageIn__info pageIn__info_gap_a">
+         <div class="pageIn__infoIt">Subject <strong>First Meeting</strong></div>
+         <div class="pageIn__infoIt">Date  <strong>Tuesday, September 10, 2019</strong></div>
+         <div class="pageIn__infoIt"> Time <strong>9:00am</strong></div>
+         <div class="pageIn__infoIt"> Location <strong><a href="#">Room 535</a>, State Capitol Building</strong></div>
+      </div>
+
+        <div class="bWrap bWrap_size_a bWrap_cl_a bWrap_f_a bWrap_gap_a">
+          <ol>
+            <li>
+              <p>
+                <strong>9:00 a.m. - 11:00 a.m. - IS 19-66, Senator Gary Stanislawski, Study on Updating HB 1017 School Mandates</strong>
+              </p>
+
+              <p>
+                Carolyn Thompson, chief of government affairs, State Department of Education <br>
+                Michelle Exstrom, education group director, and Daniel Thatcher, education program director, National Conference of State Legislatures
+                <br>
+                Pam Deering, executive director, Cooperative Council for Oklahoma School Administration
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <strong>1:15 p.m. - 3:00 p.m. - IS 19-16, Senator Carri Hicks, Study on Professional Development Requirements for School Districts
+                </strong>
+              </p>
+
+              <p>State Department of Education <br>
+                Pam Deering, executive director, Cooperative Council for Oklahoma School Administration <br>
+                Julie James, director of professional development, Oklahoma City Public Schools <br>
+                Sherri Pankhurst, director of curriculum and instruction, Cordell Public Schools</p>
+            </li>
+
+            <li>
+              <p>
+                <strong>3:00 p.m. - 4:30 p.m. - IS 19-6, Senator Mary Boren, Study on Textbook Adoption and Funding Reforms
+                </strong>
+              </p>
+
+              <p>
+                Kim Gillert, member, Oklahoma Education Publishers Association <br>
+                John Thompson, president, Thompson School Book Depository<br>
+                Cynthia Barber, Tulsa Public Schools<br>
+                Jon Myers, Noble Public Schools<br>
+                Kate Raymond, professor, University of Oklahoma Jeannine Rainbolt College of Education<br>
+                Tiffany Neill, executive director of curriculum and instruction, State Department of Education
+              </p>
+
+              <p>
+                <strong>Education Committee Members:</strong>
+              </p>
+
+              <p>
+                Senator Gary Stanislawski, Chair<br>
+                Senator Joe Newhouse, Vice-Chair<br>
+                Senator David Bullard<br>
+                Senator JJ Dossett<br>
+                Senator Tom Dugger<br>
+                Senator John Haste<br>
+                Senator Carri Hicks<br>
+                Senator Allison Ikley-Freeman<br>
+                Senator Chris Kidd<br>
+                Senator Roland Pederson<br>
+                Senator Dewayne Pemberton<br>
+                Senator Marty Quinn<br>
+                Senator Paul Scott<br>
+                Senator Wayne Shaw<br>
+                Senator Jason Smalley<br>
+              </p>
+
+              <p>
+                <strong>Studies Requested By:</strong>
+              </p>
+
+              <p>
+                Senator Mary Boren for 19-6 <br>
+                Senator Carri Hicks for 19-16<br>
+                Senator Gary Stanislawski for 19-66<br>
+              </p>
+
+              <p>
+                <strong>Senate Staff:</strong>
+              </p>
+
+              <p>
+                Erin Boeckman, Legislative Analyst<br>
+                Chris Turner, Attorney<br>
+                Leigh Garrison, Fiscal Analyst<br>
+                Kaycee Valencia, Admin. Assist.
+              </p>
+
+            </li>
+
+          </ol>
+        </div>
+
+        <div class="bWrap bWrap_size_a bWrap_cl_a bWrap_f_a">
+          <div class="bTitle bTitle_wSub_a bTitle_gap_b">
+            <h2>Meeting Related Materials</h2>
+            <div class="bTitle__sub bTitle__sub_b">
+              September 10, 2019
+            </div>
+          </div>
+
+          <div class="bColumns bColumns_2 bColumns_linkOnly">
+            <a href="#">Meeting Notice</a><a href="#">Summary Report of Failure to Launch 2019</a><a href="#">Lesson
+              Sources for Oklahoma Math Teachers</a><a
+              href="#">John Thompson Book Depository</a><a href="#">Failure to Launch</a><a href="#">Statute Related to
+              Textbooks</a><a
+              href="#">Gillert Testimony</a><a href="#">Gillert Presentation</a><a href="#">OSDE - Professional
+              Development</a><a href="#">Pankhurst Powerpoint</a><a
+              href="#">OKCPS Presentation</a><a
+              href="#">OKCPS - Professional Development</a><a
+              href="#">OSDE - Image 1</a><a href="#">OSDE - Image 2</a><a href="#">OPSRC - Presentation</a><a
+              href="#">OSDE - Textbook Adoption</a><a href="#">Oklahoma Instructional Expenditures by Category FY 11 to FY
+              18</a><a
+              href="#">Professional Development Requirements</a>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
