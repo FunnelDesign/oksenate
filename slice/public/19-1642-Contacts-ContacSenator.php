2d6fab0eb4df68f778db94f95d18fc4e00268e8a
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-11-27 13:53:33 +0300

    #112093 Develop Contacts > Contact My Senator

---
+++ slice/public/19-1642-Contacts-ContacSenator.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Contacts-ContacSenator'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_j section_bg-color_c">
+
+
+      <div class="section__sNav section__sNav_a">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_b link_back">back</a>
+          <div class="breadcrumb breadcrumb_a">
+            <a href="#">Contacts</a>
+            <span>Contact My Senator</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="bContainer">
+
+        <div class="pageIn__ico pageIn__ico_b">
+          <img src="../dist/images/titleIco/title-ico-21.png" srcset="../dist/images/titleIco/title-ico-21-2x.png 2x" alt="">
+        </div>
+        <div class="bTitle bTitle_wSub_a bTitle_gap_b">
+          <h1>Contact My Senator</h1>
+        </div>
+
+        <div class="form fSignUp">
+
+          <div class="fSignUp__desc">
+            <p>Select your senator below and then fill in <br>
+              the fields to send your senator a message.</p>
+          </div>
+          <div class="fSignUp__items">
+
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <input type="text" class="form-text" placeholder="Your Name"/>
+            </div>
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <input type="text" class="form-text" placeholder="Your Address"/>
+            </div>
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <input type="text" class="form-text" placeholder="Your City"/>
+            </div>
+            <div class="am-row">
+              <div class="am-col-sm-6 form-item form-type-select">
+                <select class="form-select">
+                  <option>Your State</option>
+                  <option>State</option>
+                  <option>State</option>
+                  <option>State</option>
+                  <option>State</option>
+                  <option>State</option>
+                  <option>State</option>
+                  <option>State</option>
+                </select>
+              </div>
+
+              <div class="am-col-sm-6 form-item form-type-text">
+                <label class="form-required"></label>
+                <input type="text" class="form-text" placeholder="Your ZipCode"/>
+              </div>
+
+            </div>
+
+            <div class="form-item form-type-text">
+              <label></label>
+              <input type="text" class="form-text" placeholder="Phone Number"/>
+            </div>
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <input type="text" class="form-text" placeholder="Your Email Address"/>
+            </div>
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <input type="text" class="form-text" placeholder="Subject"/>
+            </div>
+
+            <div class="form-item form-type-select">
+              <select class="form-select">
+                <option>Send Email To</option>
+                <option>Send Email To</option>
+                <option>Send Email To</option>
+                <option>Send Email To</option>
+                <option>Send Email To</option>
+                <option>Send Email To</option>
+              </select>
+            </div>
+
+            <div class="form-item form-type-text">
+              <label class="form-required"></label>
+              <textarea class="form-textarea" placeholder="Your Message"></textarea>
+            </div>
+          </div>
+          <div class="captcha">
+            <img src="../dist/images/tmp/captcha.jpg" alt="">
+          </div>
+
+          <div class="captcha"><input data-drupal-selector="edit-captcha-sid" type="hidden" name="captcha_sid" value="31269">
+            <input data-drupal-selector="edit-captcha-token" type="hidden" name="captcha_token" value="f19d80619d3bc41f1b2e5a824b14f5a0">
+            <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-captcha-response form-item-captcha-response">
+              <label for="edit-captcha-response" class="js-form-required form-required">Math question</label>
+              <span class="field-prefix">1 + 2 =</span>
+              <input autocomplete="off" data-drupal-selector="edit-captcha-response" aria-describedby="edit-captcha-response--description" type="text" id="edit-captcha-response" name="captcha_response" value="" size="4" maxlength="2" class="form-text required" required="required" aria-required="true">
+
+              <div id="edit-captcha-response--description" class="description">
+                Solve this simple math problem and enter the result. E.g. for 1+3, enter 4.
+              </div>
+            </div>
+          </div>
+
+          <div class="form-actions">
+            <input type="submit" class="form-submit" value="submit">
+            <div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>
+          </div>
+        </div>
+
+        <div class="webform-confirmation__message">
+          <p>
+            Your form was successfully submitted!
+          </p>
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
