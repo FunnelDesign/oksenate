d0e071990aab3dcda7916cf1eff9ab49d0cb6042
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-04 16:19:40 +0300

    loader

--- slice/src/components/Observer/CustomEv.js
+++ slice/src/components/Observer/CustomEv.js
@@ -4,7 +4,6 @@ class CustomEv {
 
 	constructor() {
 		this.addAccessibilityCookie();
-		this.menuMobileOpen();
 		this.customClick();
 	}
 
@@ -12,6 +11,8 @@ class CustomEv {
 
 		$('#views-exposed-form-senators-page-1 .form-submit').on('click touch', ()=> {
 			console.log('filter submit');
+
+			$('.sSen .sSen__res').addClass('sSen__res_loading');
 		});
 
 		$(document).on('submit',function(){
@@ -22,72 +23,21 @@ class CustomEv {
 			console.log('ajaxStart event: ', event);
 			console.log('ajaxStart xhr: ', xhr);
 			console.log('ajaxStart settings: ', settings);
-
-			if ($(event.target.id) === 'views-exposed-form-senators-page-1') {
-				console.log('ajaxStart event.target.id: ',event.target.id);
-			}
 		});
 
 		$(document).ajaxComplete(function(event, xhr, settings) {
-			console.log('event: ', event);
-			console.log('xhr: ', xhr);
-			console.log('settings: ', settings);
-
-			if ($(event.target.id) === 'views-exposed-form-senators-page-1') {
-				console.log('event.target.id: ',event.target.id);
-			}
-		});
-
-		$('.sSen__head .bef-exposed-form').on('submit', function(){
-			console.log('submit');
-			$('.sSen__res').addClass('sSen__res_loading')
-
+			console.log('ajaxComplete event: ', event);
+			console.log('ajaxComplete xhr: ', xhr);
+			console.log('ajaxComplete settings: ', settings);
 		});
 
 		$(document).on('click touch', (e) => {
 			let $ev =  $(e.target);
 
-			console.log($ev);
-
-			if ($ev.hasClass('form-submit') && $ev.closest('.sSen__fil').length) {
-
-				console.log($ev, 'sSen form-submit');
-				$('.sSen__res').addClass('sSen__res_loading')
-			}
-
 			if (!$ev.closest('.bDrop').length) {
 				$('.bDrop').removeClass('select2-container--open')
 			}
 		});
-	}
-
-	menuMobileOpen() {
-		let clName = 'mobileMenu';
-		let $elm = $(`.sHeader`);
-
-		$elm.on(`ev.toggleClass.${clName}`, (e) => {
-			$('body').toggleClass(`${clName}-open`);
-		});
-
-		$elm.on(`ev.removeClass.${clName}`, (e) => {
-			$('body').removeClass(`${clName}-open`);
-		});
-
-		// var ro = new ResizeObserver( entries => {
-		// 	for (let entry of entries) {
-		// 		const cr = entry.contentRect;
-		// 		console.log('Element:', entry.target);
-		// 		console.log(`Element size: ${cr.width}px x ${cr.height}px`);
-		// 		console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
-		//
-		// 		if ($elm.hasClass(`sHeader_fixed`)) {
-		// 			$('.pageWr').css('padding-top', `${cr.height}px`)
-		// 		}
-		// 	}
-		// });
-
-// Observe one or multiple elements
-/*		ro.observe($elm[0]);*/
 	}
 
 	addAccessibilityCookie() {
