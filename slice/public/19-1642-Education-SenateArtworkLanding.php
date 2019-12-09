2f72b1fa86b790c87fbd753c39764f21b364f2da
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-09 14:58:48 +0300

    #112244 Develop Education > Art Work landing page

---
+++ slice/public/19-1642-Education-SenateArtworkLanding.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education-SenateArtworkLanding'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Education</a>
+            <span>Senate Artwork</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_c">
+        <div class="bContainer">
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-41.png"
+                 srcset="../dist/images/titleIco/title-ico-41-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+            <h1>Senate Artwork</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="bTitle bTitle_gap_f">
+          <div class="bTitle__sub bTitle__sub_c">
+            <p>Explore the wonderful artwork on display around the <br>
+              Oklahoma Senate.</p>
+          </div>
+        </div>
+
+        <div class="bTiles bTiles_art">
+
+          <div class="bTiles__items">
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-1.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+                Art Treasures of the Oklahoma State Capitol
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-2.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+                101 Ranch
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-3.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+               45th Infantry at Pork Chop Hill, Korea
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-4.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              Arcadia Round Up
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-5.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              Cassius M. Barnes
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-6.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              Battle of Round Mountain
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-7.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              David Ross Boyd
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-8.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              Bronze Roundels
+              </span>
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__imgWrap">
+                <span class="bTiles__img"
+                      style="background-image: url('../dist/images/art/art-img-tmp-9.jpg')"></span>
+              </span>
+              <span class="bTiles__title">
+              Buffalo Skinner’s Cart
+              </span>
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
