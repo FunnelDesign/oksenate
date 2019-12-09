4ff88af35f28373eccef6cf6c2df1b5813f954ab
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-09 13:58:50 +0300

    #112245 Develop Education > Individual art work page

---
+++ slice/public/19-1642-Education-SenateArtworkIndividual.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education-SenateArtworkIndividual'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_j section_bg-color_c">
+      <div class="section__sNav section__sNav_a">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_b link_back">back</a>
+          <div class="breadcrumb breadcrumb_a">
+            <a href="#">Education</a>
+            <a href="#">Senate Artwork</a>
+            <span>Art Treasures of the Oklahoma State Capitol</span>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="bContainer">
+        
+        <div class="bPost bPost_с">
+          <div class="bPost__header">
+            <div class="bPost__title">
+              <h1>Art Treasures of the Oklahoma State Capitol</h1>
+            </div>
+          </div>
+          <div class="bPost__body">
+            <div class="bPost__imgWrap">
+              <div class="bPost__img">
+                <img src="../dist/images/tmp/art--tmp-img-1.jpg" alt="">
+                <span class="bPost__imgCap">
+                  180 pages <br>
+                  Coffee table book style with full color photographs <br>
+                  Anecdotes about the history of the works and artists <br>
+                  Publication Date: November 1, 2003 <br>
+                  Price: $39.95 - Regular Edition;  $100.00 - <br>
+                  Linen-bound Special Edition
+                </span>
+              </div>
+            </div>
+            <div class="bPost__btnWrap">
+              <a href="" class="btn">order form</a>
+            </div>
+            <div class="bPost__text">
+              <p>From monumental bronze sculptures that adorn its plaza, to magnificent murals, portraits and paintings that bring vibrancy to its interior, the Oklahoma State Capitol is rich with more than 100 works of art, each with its unique story of diversity and tradition.  A celebration of this art has been documented for the first time in Art Treasures of the Oklahoma State Capitol.  This book traces the histories of the capitol and the permanent artwork that glows beneath the majestic new dome.</p>
+              <p>An Oklahoma Centennial Project, this book is a masterful collaboration of the Oklahoma State Senate Historical Preservation Fund, Inc., the Oklahoma Arts Council, the Oklahoma Heritage Association and Friends of the Capitol, Inc.  All proceeds of the book will be used for the conservation, maintenance, and commission of works for the State Capitol.  The authors present it as a cultural legacy to future generations and as a beautiful tribute to the talented artists whose creativity has enhanced "the People's Capitol."</p>
+
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+
+</div>
+</body>
+</html>
