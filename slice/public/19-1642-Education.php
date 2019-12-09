bd16d2d4430273c8694aa8d18937f45cd35fc4eb
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-09 19:41:58 +0300

    #112242 Develop Education landing page (not committee related)

---
+++ slice/public/19-1642-Education.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-16.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-43.png"
+                 srcset="../dist/images/titleIco/title-ico-43-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Education</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        
+        <h2>An Overview of the Oklahoma Senate</h2>
+
+        <div class="bWrap bWrap_f_a bWrap_f_a_15 bWrap_cl_b bWrap_size_d bWrap_gap_d">
+          <p>
+            There are two houses of the Legislature of Oklahoma with the Oklahoma Senate representing the upper house, while the other is the Oklahoma House of Representatives. Set by the Oklahoma Constitution, there are a total of 48 senators. The chart below shows how those seats are represented by political party.
+          </p>
+        </div>
+
+        <div class="bSeats bSeats_gap_a">
+          <div class="bSeats__img">
+            <img src="../dist/images/tmp/seats-tmp-img-1.png"
+                 srcset="../dist/images/tmp/seats-tmp-img-1-2x.png 2x"
+                 alt="">
+
+            <div class="bSeats__cap">
+              <span>48</span>
+              Seats
+            </div>
+          </div>
+
+          <div class="bSeats__count">
+            <div class="bSeats__countD">
+              <span>9</span>
+              Democrat
+            </div>
+            <div class="bSeats__countR">
+              <span>39</span>
+              Republican
+            </div>
+          </div>
+        </div>
+
+
+        <div class="bWrap bWrap_f_a bWrap_f_a_15 bWrap_cl_b bWrap_size_d bWrap_gap_b">
+          <p>
+            Approving or rejecting gubernatorial appointments is crucial part of what being a Senator is all about. They all contribute to the creation of both state law and an annual state budget. They aid in drawing new boundaries for Oklahoma’s electoral districts every ten years, as well as serve as a court of impeachment.
+          </p>
+          <p>
+            The presiding officer of the Senate is the Lieutenant Governor of Oklahoma, who is the President of the Senate. Since the 1960s, the President Pro Tempore of the Senate has presided over daily work. Prior to that time, the President of the Senate took a leading role in the Senate, including appointing committees and members to those committees. The President of the Senate may cast a vote only in the instance of a tie vote and may not vote to create a tie.
+          </p>
+        </div>
+
+        <div class="pageIn__btnWr pageIn__btnWr_gap_a">
+          <a href="#" class="btn btn_c btn_s_j">
+            <span class="btn__ico">
+              <img src="../dist/images/link/link-ico-tmp-1.png"
+                   srcset="../dist/images/link/link-ico-tmp-1-2x.png 2x"
+                   alt="">
+            </span>
+
+            Learn About Senate History
+          </a>
+        </div>
+
+        <div class="bTiles bTiles_rel bTiles_rel_c bTiles_gap_a">
+
+          <div class="bTiles__items">
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico bTiles__ico_a">
+                <img src="../dist/images/ico/ico-tmp-10.png"
+                     srcset="../dist/images/ico/ico-tmp-10-2x.png 2x"
+                     alt="">
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn  btn_s_d">
+                find bills in progress
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico bTiles__ico_a">
+                <img src="../dist/images/ico/ico-tmp-11.png"
+                     srcset="../dist/images/ico/ico-tmp-11-2x.png 2x"
+                     alt="">
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn  btn_s_d">
+             contact your senator
+                </span>
+              </span>
+
+            </a>
+
+          </div>
+
+        </div>
+        
+        <h2>Get Acquainted with Legislative Terms</h2>
+
+        <div class="pageIn__btnWr">
+          <a href="#" class="btn btn_c btn_s_j">
+            <span class="btn__ico">
+              <img src="../dist/images/link/link-ico-tmp-2.png"
+                   srcset="../dist/images/link/link-ico-tmp-2-2x.png 2x"
+                   alt="">
+            </span>
+
+            See Our Glossary of Legislative Terms
+          </a>
+        </div>
+
+
+      </div>
+
+    </section>
+
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-17.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_f">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-44.png"
+                 srcset="../dist/images/titleIco/title-ico-44-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_gap_b">
+            <h2>Explore Our Senate Art Gallery</h2>
+          </div>
+          
+          <div class="pageIn__btnWr">
+            <a href="#" class="btn">view senate artwork</a>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
