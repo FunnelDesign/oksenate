ca070b56ca0f6488dd7955ac1686f406e2fda844
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-09 16:54:34 +0300

    #112243 Develop Education > History page

---
+++ slice/public/19-1642-Education-SenateHistory.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education-SenateHistory'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Education</a>
+            <span>Senate History</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_c">
+        <div class="bContainer">
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-42.png"
+                 srcset="../dist/images/titleIco/title-ico-42-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+            <h1>Senate History</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section sHist section__bgSh section_ind_c section_bg-color_f">
+      <div class="section__bg-wrap sHist__bg-wrap">
+        <div class="section__bg sHist__bg" style="background-image: url('../dist/images/tmp/section-bg-img-15.jpg')"></div>
+      </div>
+
+      <div class="bContainer">
+        <div class="bTitle bTitle_gap_f">
+          <div class="bTitle__sub bTitle__sub_c bTitle__sub_cl_a bTitle__sub_s_a">
+            <p>Below are some important events that have shaped the
+              Oklahoma Senate throughout the 100-plus years of it’s existence.</p>
+          </div>
+        </div>
+
+        <div class="bHist">
+
+          <div class="bHist__its">
+
+            <div class="bHist__it">
+              <div class="bHist__date">
+                1907
+              </div>
+              <div class="bHist__desc">
+                <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
