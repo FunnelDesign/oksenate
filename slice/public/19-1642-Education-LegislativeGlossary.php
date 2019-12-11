6f6727493ad50a7b09873873833d4ba7bc78f7db
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-11 19:46:35 +0300

    #112264 Develop Education > Glossary

---
+++ slice/public/19-1642-Education-LegislativeGlossary.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education-LegislativeGlossary'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Education</a>
+            <span>Legislative Terms</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_c">
+        <div class="bContainer">
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-45.png"
+                 srcset="../dist/images/titleIco/title-ico-45-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+            <h1>Legislative Terms</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="bTitle bTitle_gap_f">
+          <div class="bTitle__sub bTitle__sub_c bTitle__sub_s_b">
+            <p>These terms are defined according to their use in the Oklahoma Legislature and in the Oklahoma State Senate.</p>
+          </div>
+        </div>
+
+        <div class="bTerms">
+          <div class="bTerms__fil">
+            <div class="bTerms__filTitle">
+              jump to letter
+            </div>
+            <div class="bTerms__filLetters">
+              <a href="#letter-a">A</a>
+              <a href="#letter-b">B</a>
+              <a href="#letter-c">C</a>
+              <a href="#letter-d">D</a>
+              <a href="#letter-e">E</a>
+              <a href="#letter-f">F</a>
+              <a href="#letter-g">G</a>
+              <a href="#letter-h">H</a>
+              <a href="#letter-i">I</a>
+              <a href="#letter-j">J</a>
+              <a href="#letter-l">L</a>
+              <a href="#letter-m">M</a>
+              <a href="#letter-n">N</a>
+              <a href="#letter-o">O</a>
+              <a href="#letter-p">P</a>
+              <a href="#letter-q">Q</a>
+              <a href="#letter-r">R</a>
+              <a href="#letter-s">S</a>
+              <a href="#letter-t">T</a>
+              <a href="#letter-u">U</a>
+              <a href="#letter-v">V</a>
+              <a href="#letter-w">W</a>
+              <a href="#letter-x">X</a>
+              <a href="#letter-y">Y</a>
+              <a href="#letter-z">Z</a>
+            </div>
+          </div>
+
+          <div class="bTerms__its">
+
+            <div class="bTerms__it" id="item-a">
+
+              <div class="bTerms__bTitle">
+                Absolute Majority
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>See “Majority.”</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Act
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>A measure which has become law after:  (1) being passed by both houses and approved by the Governor, (2) being passed over the Governor's veto, or (3) becoming effective without the Governor's signature.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Action
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>Disposition of any question before the legislature.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Adjournment
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>Termination of a daily session; occurring at the close of each legislative day upon the completion of business, with the hour and day of the next meeting being set prior to adjournment.  (See also "Sine Die".)</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Adoption
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>Approval or acceptance; usually applied to amendments, resolutions or conference committee reports.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Advancement
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>Approval or acceptance; usually applied to amendments, resolutions or conference committee reports.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Advise and Consent
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>Confirmation by the Senate of certain appointees of the Governor or other appointing authority.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Agenda (Committee)
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>a list of legislative measures, by number and short title, arranged according to the order of business, to be considered in a committee meeting.</p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it" id="item-c">
+
+              <div class="bTerms__bTitle">
+                Committee
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>A group of members of a legislative body to which is assigned a special task.</p>
+              </div>
+
+              <div class="bTerms__bSub">
+
+                <div class="bTerms__subIt">
+                  <div class="bTerms__bTitle">
+                    Standing Committee
+                  </div>
+                  <div class="bTerms__bBody">
+                    <p>A permanent committee set up to handle legislation in a certain field.</p>
+                  </div>
+                </div>
+                <div class="bTerms__subIt">
+                  <div class="bTerms__bTitle">
+                    Special or Select Committee
+                  </div>
+                  <div class="bTerms__bBody">
+                    <p>A temporary committee set up to deal with a specific issue.</p>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                Committee Report
+              </div>
+
+              <div class="bTerms__bBody">
+                <p>The report of an action of a majority of the members of a certain com­mittee on any measure.  The committee report is transmitted to the floor.  A committee re­port shows a recommendation for action, all committee amendments and any authorship changes.  Recommendations for action are limited to "do pass" or "do pass as amended".  The latter can include a committee substitute for the measure.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it" id="item-d">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+            <div class="bTerms__it">
+
+              <div class="bTerms__bTitle">
+                D Lorem
+              </div>
+
+              <div class="bTerms__bBody">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores enim eveniet iure, nam perferendis quasi qui quisquam recusandae repellendus tempore. Delectus dolorum eos, illo magni molestias numquam porro praesentium suscipit.
+                </p>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
