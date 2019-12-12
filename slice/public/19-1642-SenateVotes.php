7dd5da3a3e3d29d38e0642f61ded3f4ca296ccf0
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-12 12:55:45 +0300

    #112267 Develop Senate votes page

---
+++ slice/public/19-1642-SenateVotes.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-SenateVotes'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-18.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-46.png"
+                 srcset="../dist/images/titleIco/title-ico-46-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Senate Votes</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <h2>57th Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2019 - 1st Session Information</a>
+          </div>
+        </div>
+
+        <h2>56th Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2017 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2017 - 1st Ext Information</a>
+            <a href="#" class="bListLinks__link">2017 - 2nd Ext Information</a>
+            <a href="#" class="bListLinks__link">2018 - 1st Ext Information</a>
+          </div>
+        </div>
+
+        <h2>55th Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2015 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2016 - 2nd Session Information</a>
+          </div>
+        </div>
+
+        <h2>54th Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2013 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2014 - 2nd Session Information</a>
+            <a href="#" class="bListLinks__link">2014 - 2nd Session Information</a>
+          </div>
+        </div>
+
+
+        <h2>53rd Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2011 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2012 - 2nd Session Information</a>
+          </div>
+        </div>
+
+        <h2>52nd Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2009 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2010 - 2nd Session Information</a>
+          </div>
+        </div>
+
+        <h2>51st Legislature</h2>
+
+        <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+          <div class="bListLinks__links">
+            <a href="#" class="bListLinks__link">2009 - 1st Session Information</a>
+            <a href="#" class="bListLinks__link">2010 - 2nd Session Information</a>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
