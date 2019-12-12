62553fea8505c2086b1b8715c81376a828869290
daniyar.seferov <daniyar.seferov@rockdevels.com>
2019-12-12 23:16:19 +0300

    #111905 Journal

---
+++ trunk/web/themes/custom/senate/dist/js/custom.js
@@ -0,0 +1,19 @@
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.senateCustom = {
+    attach: function (context, settings) {
+      $('#views-exposed-form-journals-page-1', context).once('journals-options').each(function () {
+        var month = this.elements['edit-month'];
+        var options = drupalSettings.senate.view_journals || {};
+
+        $('#views-exposed-form-journals-page-1 #edit-year').on('select2:select', function (e) {
+          var year = e.params.data.id;
+
+          var keys = Object.keys(options[year]);
+          month.innerHTML = keys.map(function (key) {
+            return `<option value="${key}">${options[year][key]}</option>`;
+          }).join();
+        });
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
