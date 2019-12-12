0be834044dfab76f2ded9d4651b0571587494716
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-12 15:34:52 +0300

    #112268 Develop Senate Votes Individual page

---
+++ slice/public/19-1642-SenateVotes-Individual.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-SenateVotes-Individual'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_j section_bg-color_c">
+
+
+      <div class="section__sNav section__sNav_a">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_b link_back">back</a>
+          <div class="breadcrumb breadcrumb_a">
+            <a href="#">Senate Votes</a>
+            <span>Oklahoma State Senate 2019 Votes</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="bContainer">
+
+        <div class="bTitle bTitle_gap_f">
+          <h1>Oklahoma State Senate 2019 Votes</h1>
+        </div>
+
+        <div class="bWrap bWrap_f_a bWrap_f_a_15 bWrap_cl_b bWrap_size_f bWrap_gap_d">
+          <p>
+            The information shown on this page is provided for the benefit of the public and shows the results of roll call votes taken on the Senate floor with the use of the electronic voting machine.  It may not reflect other actions relating to roll call votes for which the voting machine is not used, such as a member taking his or her constitutional privilege not to vote on a measure, a member shown as a “no” vote if in the chamber and not voting, a member shown as “excused”, or the Lieutenant Governor exercising a casting vote.  Please consult the Senate Journal for official information on roll call votes and other actions taken by the Senate.
+          </p>
+        </div>
+
+        <div class="bTb bTb_a bTb_a_vo">
+
+          <div class="bTb__tHead">
+            <div class="bTb__tr">
+
+              <div class="bTb__th">
+                date
+              </div>
+
+              <div class="bTb__th">
+                measure
+              </div>
+
+              <div class="bTb__th">
+                author
+              </div>
+
+              <div class="bTb__th">
+                action
+              </div>
+
+              <div class="bTb__th">
+                yeas
+              </div>
+
+              <div class="bTb__th">
+                NAYS
+              </div>
+
+            </div>
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB1
+              </div>
+
+              <div class="bTb__td">
+                Treat
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>5/21/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB1
+              </div>
+
+              <div class="bTb__td">
+                Treat
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">fourth reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB3
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB4
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB4
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB4
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="bTb__tBody">
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB4
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+            <div class="bTb__tr">
+
+              <div class="bTb__td">
+                <strong>3/11/2019</strong>
+              </div>
+
+              <div class="bTb__td">
+                SB4
+              </div>
+
+              <div class="bTb__td">
+                Rosino
+              </div>
+
+              <div class="bTb__td">
+                <a href="#" class="bTb__text bTb__text_a pageIn_cl_c">third reading</a>
+              </div>
+
+              <div class="bTb__td">
+                <strong>38</strong>
+              </div>
+
+              <div class="bTb__td">
+                <strong>10</strong>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
