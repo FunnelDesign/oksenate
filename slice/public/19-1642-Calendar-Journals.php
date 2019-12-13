4b688c8a910acd70757edc026be96c5b12fb50b0
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-13 13:05:58 +0300

    journals fix

--- slice/public/19-1642-Calendar-Journals.php
+++ slice/public/19-1642-Calendar-Journals.php
@@ -41,29 +41,50 @@
 
       <div class="bContainer">
 
-        <div class="bSelect bSelect_a bSelect_size_b  bSelect_gap_b">
-          <div class="form-item form-type-select">
-            <select class="form-select">
-              <option>2019</option>
-              <option>2019</option>
-              <option>2019</option>
-              <option>2019</option>
-              <option>2019</option>
-              <option>2019</option>
-            </select>
-          </div>
+<!--        <div class="bSelect bSelect_a bSelect_size_b  bSelect_gap_b">-->
+          <form class="views-exposed-form bSelect bSelect_a bSelect_size_d  bSelect_gap_b" data-drupal-selector="views-exposed-form-journals-page-1" action="/calendar/journals" method="get" id="views-exposed-form-journals-page-1" accept-charset="UTF-8">
+            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-year form-item-year form-no-label">
+              <select data-drupal-selector="edit-year" id="edit-year" name="year" class="form-select">
+                <option value="2019">2019</option>
+                <option value="2018" selected="selected">2018</option>
+              </select>
+            </div>
+            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-month form-item-month form-no-label">
+              <select data-drupal-selector="edit-month" id="edit-month" name="month" class="form-select">
+                <option value="02">February</option>
+                <option value="03" selected="selected">March</option>
+              </select>
+            </div>
+            <div data-drupal-selector="edit-actions" class="form-actions js-form-wrapper form-wrapper" id="edit-actions">
+              <input data-drupal-selector="edit-submit-journals" type="submit" id="edit-submit-journals" value="Apply" class="button js-form-submit form-submit"/>
+            </div>
+          </form>
 
-          <div class="form-item form-type-select">
-            <select class="form-select">
-              <option>January</option>
-              <option>January</option>
-              <option>January</option>
-              <option>January</option>
-              <option>January</option>
-              <option>January</option>
-            </select>
-          </div>
-        </div>
+<!--        </div>-->
+
+<!--        <div class="bSelect bSelect_a bSelect_size_b  bSelect_gap_b">-->
+<!--          <div class="form-item form-type-select">-->
+<!--            <select class="form-select">-->
+<!--              <option>2019</option>-->
+<!--              <option>2019</option>-->
+<!--              <option>2019</option>-->
+<!--              <option>2019</option>-->
+<!--              <option>2019</option>-->
+<!--              <option>2019</option>-->
+<!--            </select>-->
+<!--          </div>-->
+<!---->
+<!--          <div class="form-item form-type-select">-->
+<!--            <select class="form-select">-->
+<!--              <option>January</option>-->
+<!--              <option>January</option>-->
+<!--              <option>January</option>-->
+<!--              <option>January</option>-->
+<!--              <option>January</option>-->
+<!--              <option>January</option>-->
+<!--            </select>-->
+<!--          </div>-->
+<!--        </div>-->
 
         <div class="bTitle bTitle_wSub_a bTitle_gap_b">
           <h1>2019 Senate Journals</h1>
@@ -72,7 +93,7 @@
           </div>
         </div>
 
-        <div class="bListLinks bListLinks_a">
+        <div class="bListLinks bListLinks_a bListLinks_gap_a">
           <div class="bListLinks__items">
             <a href="#" class="btn btn_b btn_s_b">january 3 pdf</a>
             <a href="#" class="btn btn_b btn_s_b">january 4 pdf</a>
@@ -87,6 +108,19 @@
             <a href="#" class="btn btn_b btn_s_b">january 13 pdf</a>
             <a href="#" class="btn btn_b btn_s_b">january 14 pdf</a>
             <a href="#" class="btn btn_b btn_s_b">january 15 pdf</a>
+          </div>
+        </div>
+
+        <div class="bTitle bTitle_wSub_a bTitle_gap_b">
+          <h1>2019 Senate Journals</h1>
+          <div class="bTitle__sub bTitle__sub_a">
+            1st Session - 57th Legislature
+          </div>
+        </div>
+
+        <div class="bListLinks bListLinks_a bListLinks_gap_a">
+          <div class="bListLinks__items">
+            <a href="#" class="btn btn_b btn_s_b">january 3 pdf</a>
           </div>
         </div>
 
