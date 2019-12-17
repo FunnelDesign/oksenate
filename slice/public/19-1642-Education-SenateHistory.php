94407a4de2e35459996b596bb6e768b90aaab94b
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2019-12-17 15:22:15 +0300

    #44682 Populate senate history

--- slice/public/19-1642-Education-SenateHistory.php
+++ slice/public/19-1642-Education-SenateHistory.php
@@ -62,64 +62,66 @@
                 </div>
               </div>
               <div class="bHist__desc">
-                <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                <div class="bHist__descIn">
+                  <div class="bHist__time">
+                    November 27, 1907
+                  </div>
+                  <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                </div>
+              </div>
+              <div class="bHist__desc">
+                <div class="bHist__descIn">
+                  <div class="bHist__time">
+                    December 12, 1907
+                  </div>
+                  <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                </div>
+
               </div>
             </div>
 
             <div class="bHist__it">
               <div class="bHist__header">
                 <div class="bHist__date">
-                  1920
+                  1908
                 </div>
               </div>
               <div class="bHist__desc">
-                <p>After women in Oklahoma earned the right to vote in 1918, the Oklahoma Senate gained its first female state senator. Lamar Looney was elected in 1920 over a male incumbent, G. L. Wilson. Looney was a progressive Democrat and served from 1921 to 1929.</p>
+                <div class="bHist__descIn">
+                  <div class="bHist__time">
+                    April 9, 1908
+                  </div>
+                  <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                </div>
               </div>
             </div>
 
             <div class="bHist__it">
               <div class="bHist__header">
                 <div class="bHist__date">
-                  1962
+                  1912
                 </div>
               </div>
               <div class="bHist__desc">
-                <p>The United States Supreme Court "one man, one vote" decision in Baker v. Carr led to a court order that forced Oklahoma to equalize representation. Before that decision, Oklahoma had 48 senatorial districts that represented either a populous county or several less-populated counties, but did not provide for districts of equal population.</p>
+                <div class="bHist__descIn">
+                  <div class="bHist__time">
+                    November 2, 191
+                  </div>
+                  <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                </div>
               </div>
+
+              <div class="bHist__desc">
+                <div class="bHist__descIn">
+                  <div class="bHist__time">
+                    November 8, 1912
+                  </div>
+                  <p>The 1907 Oklahoma Constitution established the Oklahoma Senate alongside the Oklahoma House of Representatives. It met in Guthrie, Oklahoma until 1910. Henry S. Johnston, the author of the initiative and referendum section of the Oklahoma Constitution, served as the first Senate President Pro Tempore.</p>
+                </div>
+              </div>
+
             </div>
 
-            <div class="bHist__it">
-              <div class="bHist__header">
-                <div class="bHist__date">
-                  1964
-                </div>
-              </div>
-              <div class="bHist__desc">
-                <p>Since 1964, under the holding of Reynolds v. Sims, 377 U.S. 533 (1964) districts must be apportioned within a 5% margin of the average target size district as determined by the U.S. Census state population figures divided by the forty-eight districts. This allows for some districts to be slightly smaller or larger than others. The Oklahoma Senate draws its own maps of its district lines, which are subject to the approval of both the Oklahoma House of Representatives and the governor. </p>
-              </div>
-            </div>
-
-            <div class="bHist__it">
-              <div class="bHist__header">
-                <div class="bHist__date">
-                  1989
-                </div>
-              </div>
-              <div class="bHist__desc">
-                <p>An initiative petition championed by Governor Henry Bellmon in 1989 created a requirement that the legislative sessions end by 5 p.m. on the last Friday in May</p>
-              </div>
-            </div>
-
-            <div class="bHist__it">
-              <div class="bHist__header">
-                <div class="bHist__date">
-                  2006
-                </div>
-              </div>
-              <div class="bHist__desc">
-                <p>The November 7, 2006 elections resulted in an unprecedented 24–24 tie in the number of seats held by Oklahoma's two major political parties, the Republican Party and the Democratic Party. Although the Republican Party added two seats to their prior total,  they had lost a seat in July due to Nancy Riley changing in her party affiliation from Republican to Democratic. The Democratic Party did hold the seat of lieutenant governor, who also serves as President of the Senate, giving them a tie-breaking vote in the Senate.</p>
-              </div>
-            </div>
 
           </div>
         </div>
