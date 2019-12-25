eb19407ecafe3f234126c4d157bf478f3c51bcc6
dmitry.drobyshevsky <drobyshevsky.d@gmail.com>
2019-12-25 19:15:50 +0300

    Week review import

--- trunk/web/modules/custom/import_audio/src/Controller/TestController.php
+++ trunk/web/modules/custom/import_audio/src/Controller/TestController.php
@@ -29,4 +29,37 @@ class TestController extends ControllerBase {
     ];
   }
 
+  public function testWeekImport() {
+
+    $web_week_urls = \Drupal::state()->get('import_audio.web_week_urls');
+    if(empty($web_week_urls)) {
+      $web_week_urls =  WeekImportBatch::getWeekUrls();
+      \Drupal::state()->set('import_audio.web_week_urls', $web_week_urls);
+    }
+
+
+    $out = '<h2>Not found</h2>';
+
+    $urls = \Drupal::database()->select('node__field_old_url', 'u')
+      //->fields('u')
+      ->fields('u', ['field_old_url_value'])
+      ->execute()
+      //->fetchCol();
+      ->fetchCol();
+
+
+    dsm(array_diff($web_week_urls, $urls), 'NOT FOUND');
+
+    dsm($web_week_urls, 'web');
+
+    dsm($urls);
+
+
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $out,
+    ];
+
+  }
 }
