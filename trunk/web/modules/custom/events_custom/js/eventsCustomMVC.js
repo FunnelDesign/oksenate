c772f849eae06e3e097f2a7575adc820dd8e73db
daniyar.seferov <daniyar.seferov@rockdevels.com>
2020-02-01 22:18:25 +0300

    #113509 Meeting notices

--- trunk/web/modules/custom/events_custom/js/eventsCustomMVC.js
+++ trunk/web/modules/custom/events_custom/js/eventsCustomMVC.js
@@ -2,10 +2,6 @@
 /* ======= Model ======= */
 
 var modelEvents = {
-  EVENT_NOT_STARTED: 0,
-  EVENT_STARTED: 1,
-  EVENT_ADJOURNED: -1,
-
   events: []
 };
 
@@ -13,11 +9,22 @@ var modelEvents = {
 /* ======= Controller ======= */
 
 var controllerEvents = {
+  apiUrl: 'https://sg001-harmony.sliq.net/00282/Harmony/en/api/Data/GetCompleteMeeting?meetingId=',
+  EVENT_OPENED: -4,
+  EVENT_CANCELLED: -3,
+  EVENT_END_PENDING: -2,
+  EVENT_ENDED: -1,
+  EVENT_NOT_STARTED: 0,
+  EVENT_STARTED: 1,
+  EVENT_PAUSED: 2,
+  EVENT_IN_CAMERA: 101,
+  EVENT_IN_CAMERA_PAUSED: 102,
 
   init: function() {
     if (drupalSettings.hasOwnProperty('events_custom')) {
       var eventsTimetable = drupalSettings.events_custom.timetable || {};
       var now = moment.utc();
+      var _this = this;
 
       eventsTimetable.filter(function (event) {
         var eventEnd = moment.utc(event.endUtc);
@@ -26,13 +33,14 @@ var controllerEvents = {
         var eventStart = moment.utc(event.startUtc);
         var eventEnd = moment.utc(event.endUtc);
 
-        event.status = modelEvents.EVENT_NOT_STARTED;
+        event.status = _this.EVENT_NOT_STARTED;
         event.statusChanged = false;
 
         if ((eventStart <= now) && (eventEnd >= now)) {
-          event.status = modelEvents.EVENT_STARTED;
-          event.statusChanged = true;
+          var url = _this.apiUrl + event.meetingId;
+          _this.getEventInfo(url, _this.setEventStatus, event);
         }
+
         modelEvents.events[event.nid]= event;
       });
     }
@@ -46,34 +54,69 @@ var controllerEvents = {
   },
 
   eventLoop: function () {
+    var _this = this;
+
     setInterval(function () {
       var now = moment.utc();
       var eventNids = Object.keys(modelEvents.events);
-      var hasChangedEvents = false;
 
       eventNids.forEach(function (nid) {
         var event = modelEvents.events[nid];
         var eventStart = moment.utc(event.startUtc);
         var eventEnd = moment.utc(event.endUtc);
+        var isOnline = _this.checkEventStatusOnline(event);
+        var url = _this.apiUrl + event.meetingId;
 
         if ((eventStart <= now) && (eventEnd >= now)) {
-          if (event.status !== modelEvents.EVENT_STARTED) {
-            event.status = modelEvents.EVENT_STARTED;
-            event.statusChanged = true;
-            hasChangedEvents = true;
+          if (!isOnline) {
+            _this.getEventInfo(url, _this.setEventStatus, event);
           }
         }
-        else if (event.status === modelEvents.EVENT_STARTED) {
-          event.status = modelEvents.EVENT_ADJOURNED;
-          event.statusChanged = true;
-          hasChangedEvents = true;
+        else if (isOnline) {
+          _this.getEventInfo(url, _this.setEventStatus, event);
         }
       });
-
-      if (hasChangedEvents) {
-        viewEvents.render();
-      }
     }, 10000);
+  },
+
+  getEventInfo: function(url, clb, event) {
+    if(!clb) {clb = function() {console.warn('Use callback!')}}
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText);
+        return clb(data, event);
+      } else {
+        console.warn('We reached our target server, but it returned an error');
+      }
+    };
+
+    request.onerror = function() {
+      console.warn('There was a connection error of some sort');
+    };
+
+    request.send();
+  },
+
+  setEventStatus: function (data, event) {
+    var eventStatus = data.EntityStatus || controllerEvents.EVENT_NOT_STARTED;
+
+    if (event.status !== eventStatus) {
+      event.status = eventStatus;
+      event.statusChanged = true;
+
+      viewEvents.render();
+    }
+  },
+
+  checkEventStatusOnline: function (event) {
+    return (event.status === this.EVENT_STARTED) ||
+      (event.status === this.EVENT_PAUSED) ||
+      (event.status === this.EVENT_IN_CAMERA) ||
+      (event.status === this.EVENT_IN_CAMERA_PAUSED);
   }
 };
 
@@ -97,7 +140,8 @@ var viewEvents = {
       var event = events[eventElemNid] || {};
 
       if (event.statusChanged) {
-        if (event.status === modelEvents.EVENT_STARTED) {
+        var isOnline = controllerEvents.checkEventStatusOnline(event);
+        if (isOnline) {
           _this.setStarted(eventElem);
         }
         else {
