9a67d65e21e96fe6d4d0d2c63524fc3dba036338
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2020-02-06 12:41:38 +0300

    #113565 Make changes to internship form

--- slice/src/index.js
+++ slice/src/index.js
@@ -225,13 +225,47 @@ function initFormRedirect() {
 }
 
 function initSelect() {
+	function matchCustom(params, data) {
+		// If there are no search terms, return all of the data
+		if ($.trim(params.term) === '') {
+			return data;
+		}
+
+		// Do not display the item if there is no 'text' property
+		if (typeof data.text === 'undefined') {
+			return null;
+		}
+
+		// `params.term` should be the term that is used for searching
+		// `data.text` is the text that is displayed for the data object
+		// if (data.text.indexOf(params.term) > -1) {
+		//   var modifiedData = $.extend({}, data, true);
+		//   modifiedData.text += ' (matched)';
+		//
+		//   // You can return modified objects from here
+		//   // This includes matching the `children` how you want in nested data sets
+		//   return modifiedData;
+		// }
+
+		if (data.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0) {
+			var modifiedData = $.extend({}, data, true);
+
+			return modifiedData;
+		}
+
+		// Return `null` if the term should not be displayed
+		return null;
+	}
+
 	$('select:not(#edit-permanent-state):not(#edit-local-state)').select2({
 		width: 'full',
 		minimumResultsForSearch: Infinity
 	});
 
 	$('select#edit-permanent-state, select#edit-local-state').select2({
-		width: 'full'
+		width: 'full',
+		minimumResultsForSearch: 0,
+		matcher: matchCustom
 	});
 
 	$('select').on('select2:open', function (e) {
