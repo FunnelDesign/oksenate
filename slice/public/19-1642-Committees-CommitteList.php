0805c0456c4328000c59c6ce55da1b96b5a0fabb
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2020-02-07 20:58:11 +0300

    #113631 Develop committees list landing page

---
+++ slice/public/19-1642-Committees-CommitteList.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Committees-CommitteList'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <?php include 'tpl/blocks/bShare-no-bread.inc'; ?>
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-5.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Senate Committees</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <div class="bTiles bTiles_rel bTiles_rel_d">
+
+          <div class="bTiles__items">
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-12.png"
+                     srcset="../dist/images/ico/ico-tmp-12-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Agriculture & Wildlife
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-13.png"
+                     srcset="../dist/images/ico/ico-tmp-13-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Appropriations
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-14.png"
+                     srcset="../dist/images/ico/ico-tmp-14-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Business, Commerce and Tourism
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-15.png"
+                     srcset="../dist/images/ico/ico-tmp-15-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Education
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-16.png"
+                     srcset="../dist/images/ico/ico-tmp-16-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Energy
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-17.png"
+                     srcset="../dist/images/ico/ico-tmp-17-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Finance
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-18.png"
+                     srcset="../dist/images/ico/ico-tmp-18-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               General Government
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-19.png"
+                     srcset="../dist/images/ico/ico-tmp-19-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+               Health and Human Services
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-20.png"
+                     srcset="../dist/images/ico/ico-tmp-20-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Judiciary
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-21.png"
+                     srcset="../dist/images/ico/ico-tmp-21-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Public Safety
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-22.png"
+                     srcset="../dist/images/ico/ico-tmp-22-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Retirement and Insurance
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-23.png"
+                     srcset="../dist/images/ico/ico-tmp-23-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Rules
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-24.png"
+                     srcset="../dist/images/ico/ico-tmp-24-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Transportation
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+            <a href="#" class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/ico/ico-tmp-25.png"
+                     srcset="../dist/images/ico/ico-tmp-25-2x.png 2x">
+              </span>
+              <span class="bTiles__title">
+              Veterans and Military Affairs
+              </span>
+              <span class="bTiles__btnWrap">
+                <span class="btn btn_a">
+                view now
+                </span>
+              </span>
+
+            </a>
+
+
+
+          </div>
+
+          <div class="bTiles__btnAllWrap">
+            <a href="#" class="btn">see senate meetings</a>
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
