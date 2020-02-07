49df84797df474f38addbb3b6fed142fdf5948eb
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2020-02-07 15:42:07 +0300

    #113612 Develop meeting view page

---
+++ slice/public/room-page-2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'room-page'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+
+    <section class="section">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-4.jpg')"></div>
+      </div>
+
+<!--      <div class="bContainer">-->
+
+        <div class="bMedia bMedia_web">
+
+          <div class="bMedia__wrap">
+            <iframe name="left"
+                    scrolling="yes"
+                    src="http://sg001-harmony.sliq.net/00282/Harmony/en/PowerBrowser/RoomRouter?location=230&amp;viewMode=3"
+                    marginwidth="1"
+                    marginheight="0"
+                    target="_self"></iframe>
+          </div>
+
+          <div class="bMedia__wrap bMedia__wrap_a">
+            <iframe name="left"
+                    scrolling="yes"
+                    src="http://sg001-harmony.sliq.net/00282/Harmony/en/PowerBrowser/RoomRouter?location=230&amp;viewMode=3"
+                    marginwidth="1"
+                    marginheight="0"
+                    target="_self"></iframe>
+          </div>
+        </div>
+
+<!--      </div>-->
+
+    </section>
+
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
