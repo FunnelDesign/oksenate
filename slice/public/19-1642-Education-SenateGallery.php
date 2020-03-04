1fbc038f479447f625b2b943760ef8620dd89aaa
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2020-03-04 14:36:52 +0300

    #114567 Develop new gallery page

---
+++ slice/public/19-1642-Education-SenateGallery.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Education-SenateGallery'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-22.jpg')"></div>
+      </div>
+      <div class="section__sNav">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_a link_back">back</a>
+          <div class="breadcrumb">
+            <a href="#">Press & Media</a>
+            <span>Senate Gallery</span>
+          </div>
+          <?php include 'tpl/blocks/bShare.inc'; ?>
+        </div>
+
+      </div>
+
+      <div class="section__v-inner section__v-inner_c">
+        <div class="bContainer">
+
+          <div class="bTitle bTitle_wSub_a bTitle_gap_a">
+            <h1>Senate Gallery</h1>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <div class="bTitle bTitle_gap_f">
+          <div class="bTitle__sub bTitle__sub_c">
+            <p>See what’s been going on around the Oklahoma Senate.</p>
+          </div>
+        </div>
+
+        <div class="bTiles _gallery">
+
+          <div class="bTiles__items">
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                      style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                     style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                    >
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                     style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                     style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                     style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+            <div class="bTiles__item">
+              <div class="bTiles__imgWrap">
+                <div class="bTiles__img"
+                     style="background-image: url('../dist/images/tmp/gallery-img-1.jpg')">
+                </div>
+                <div class="bTiles__bSoc">
+
+                  <div class="bSoc _layout_a _style_simple _size_a _ind_a _color_white">
+                    <div class="bSoc__items">
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-facebook"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <span class="bSoc__ico ss-icon ss-social-circle ss-instagram"></span>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-pinterest"></a>
+                      </div>
+                      <div class="bSoc__item">
+                        <a target="_blank" href="#" class="bSoc__ico ss-icon ss-social-circle ss-twitter"></a>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
