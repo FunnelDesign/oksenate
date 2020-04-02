a104d74561c89ebd6fc6f095f80d32aac04b3044
daniyar.seferov <daniyar.seferov@rockdevels.com>
2020-04-02 13:56:52 +0300

    #115361 Joint Calendar Cron

--- trunk/web/modules/custom/senate_calendar/src/Plugin/QueueWorker/SenateCalendar.php
+++ trunk/web/modules/custom/senate_calendar/src/Plugin/QueueWorker/SenateCalendar.php
@@ -31,7 +31,6 @@ class SenateCalendar extends QueueWorkerBase {
       $files_list = $senate_calendar_helper->getListFiles($directory, $prev_execution);
       $files_list = (!empty($files_list) && is_array($files_list)) ? $files_list : [];
       $files_content = [];
-      $nodes = [];
 
       foreach ($files_list as $file) {
         $files_content[$file] = $senate_calendar_helper->getFileContentXls($file, $directory);
@@ -40,63 +39,55 @@ class SenateCalendar extends QueueWorkerBase {
           foreach ($files_content[$file] as $file_sheet) {
             if (!empty($file_sheet)) {
               $rows = !empty($file_sheet["rows"]) ? $file_sheet["rows"] : [];
+              $type = !empty($file_sheet["type"]) ? $file_sheet["type"] : '';
+              $parent_nid = $senate_calendar_helper->getLastNid($type);
+              $parent_node = !empty($parent_nid) ? Node::load($parent_nid) : NULL;
 
-              foreach ($rows as $file_row) {
-                if (!empty($file_row['action']) && !empty($file_row['action']['link'])) {
-                  $file_row['fid'] = $senate_calendar_helper->createFile($file_row['action']['link'], $directory);
-                }
+              if (!empty($parent_node)) {
+                $existing_paragraph = $senate_calendar_helper->getParagraphData($parent_node->id(), $type);
 
-                if (!empty($file_row['date'])) {
-                  $year = $senate_calendar_helper->getYear($file_row['date']);
-                  $session = !empty($file_row['session']) ? $file_row['session'] : '1st';
-                  $year_session = $year . '_' . $session;
+                foreach ($rows as $file_row) {
+                  $pid = $senate_calendar_helper->checkParagraphExists($existing_paragraph, $file_row, $type);
 
-                  if (!empty($year) && empty($nodes[$year_session])) {
-                    $parent_nid = $senate_calendar_helper->getNodeByYearSession($year, $session);
-
-                    if (!empty($parent_nid)) {
-                      $nodes[$year_session] = Node::load($parent_nid);
-                    }
-                    else {
-                      $node_data['title'] = !empty($file_sheet["title"]) ? $file_sheet["title"] : '';
-                      $node_data['description'] = !empty($file_sheet["description"]) ? $file_sheet["description"] : '';
-                      $node_data['year'] = $year;
-                      $nodes[$year_session] = $senate_calendar_helper->createNode($node_data);
-                    }
-                  }
-
-                  if (!empty($nodes[$year_session]) && is_object($nodes[$year_session])) {
-                    $parent_node = $nodes[$year_session];
-                    $existing_paragraph = $senate_calendar_helper->getVotesData($parent_node->id());
-                    $pid = $senate_calendar_helper->checkParagraphExists($existing_paragraph, $file_row);
-
-                    if (empty($pid)) {
-                      $paragraph = $senate_calendar_helper->createParagraph($parent_node, 'field_senate_calendar', $file_row);
+                  if (!$pid) {
+                    if ($type == 'cl_app_bud') {
+                      $paragraph = $senate_calendar_helper->createParagraph($parent_node, 'field_cl_app_bud_calendar', $file_row, $type, 'cl_app_budget');
 
                       if (!empty($paragraph)) {
-                        $parent_node->field_senate_calendar[] = [
+                        $parent_node->field_cl_app_bud_calendar[] = [
                           'target_id' => $paragraph->id(),
                           'target_revision_id' => $paragraph->getRevisionId(),
                         ];
                         $parent_node->save();
                       }
                     }
-//                    else {
-//                      $paragraph = $senate_calendar_helper->updateParagraph($pid, $file_row);
-//                    }
+                    else if ($type == 'cl_jnt_cnf') {
+                      $paragraph = $senate_calendar_helper->createParagraph($parent_node, 'field_cl_jnt_cnf_calendar', $file_row, $type, 'cl_jnt_cnf');
+
+                      if (!empty($paragraph)) {
+                        $parent_node->field_cl_jnt_cnf_calendar[] = [
+                          'target_id' => $paragraph->id(),
+                          'target_revision_id' => $paragraph->getRevisionId(),
+                        ];
+                        $parent_node->save();
+                      }
+                    }
+
                   }
                   else {
-                    \Drupal::logger('senate_calendar')
-                      ->error(__METHOD__ . ' ' . t('failed. Message = Parent node problem %file.', [
-                          '%file' => $file,
-                        ]));
+                    $paragraph = $senate_calendar_helper->updateParagraph($pid, $file_row, $type);
                   }
                 }
+              }
+              else {
+                \Drupal::logger('senate_calendar')
+                  ->error(__METHOD__ . ' ' . t('failed. Message = Parent node problem %file.', [
+                      '%file' => $file,
+                    ]));
               }
             }
           }
         }
-
       }
     }
   }
