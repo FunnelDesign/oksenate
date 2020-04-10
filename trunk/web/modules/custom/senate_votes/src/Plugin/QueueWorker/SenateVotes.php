2597f72e5f6951fd3d8cc8a0f9ca364d60f9a6a1
daniyar.seferov <daniyar.seferov@rockdevels.com>
2020-04-11 00:05:30 +0300

    #115494 Votes: Import

--- trunk/web/modules/custom/senate_votes/src/Plugin/QueueWorker/SenateVotes.php
+++ trunk/web/modules/custom/senate_votes/src/Plugin/QueueWorker/SenateVotes.php
@@ -40,34 +40,37 @@ class SenateVotes extends QueueWorkerBase {
           foreach ($files_content[$file] as $file_sheet) {
             if (!empty($file_sheet)) {
               $rows = !empty($file_sheet["rows"]) ? $file_sheet["rows"] : [];
+              reset($rows);
+              $first_row_data = current($rows);
+              $parent_nid = $senate_votes_helper->getNodeByFirstRowData($first_row_data);
+              $operation = '';
+              $paragraphs = [
+                'created' => [],
+                'updated' => [],
+              ];
 
-              foreach ($rows as $file_row) {
-                if (!empty($file_row['action']) && !empty($file_row['action']['link'])) {
-                  $file_row['fid'] = $senate_votes_helper->createFile($file_row['action']['link'], $directory);
-                }
+              if (!empty($parent_nid)) {
+                $parent_node = Node::load($parent_nid);
+                $operation = 'update';
+              }
+              else {
+                $node_data['title'] = !empty($file_sheet["title"]) ? $file_sheet["title"] : '';
+                $node_data['description'] = !empty($file_sheet["description"]) ? $file_sheet["description"] : '';
+                $year = !empty($first_row_data['date']) ? $first_row_data['date'] : '';
+                $node_data['year'] = $senate_votes_helper->getYear($year);
+                $parent_node = $senate_votes_helper->createNode($node_data);
+                $operation = 'create';
+              }
 
-                if (!empty($file_row['date'])) {
-                  $year = $senate_votes_helper->getYear($file_row['date']);
-                  $session = !empty($file_row['session']) ? $file_row['session'] : '1st';
-                  $year_session = $year . '_' . $session;
+              if (!empty($parent_node) && is_object($parent_node)) {
+                $existing_paragraph = $senate_votes_helper->getVotesData($parent_node->id());
 
-                  if (!empty($year) && empty($nodes[$year_session])) {
-                    $parent_nid = $senate_votes_helper->getNodeByYearSession($year, $session);
-
-                    if (!empty($parent_nid)) {
-                      $nodes[$year_session] = Node::load($parent_nid);
-                    }
-                    else {
-                      $node_data['title'] = !empty($file_sheet["title"]) ? $file_sheet["title"] : '';
-                      $node_data['description'] = !empty($file_sheet["description"]) ? $file_sheet["description"] : '';
-                      $node_data['year'] = $year;
-                      $nodes[$year_session] = $senate_votes_helper->createNode($node_data);
-                    }
+                foreach ($rows as $file_row) {
+                  if (!empty($file_row['action']) && !empty($file_row['action']['link'])) {
+                    $file_row['fid'] = $senate_votes_helper->createFile($file_row['action']['link'], $directory);
                   }
 
-                  if (!empty($nodes[$year_session]) && is_object($nodes[$year_session])) {
-                    $parent_node = $nodes[$year_session];
-                    $existing_paragraph = $senate_votes_helper->getVotesData($parent_node->id());
+                  if (!empty($file_row['date'])) {
                     $pid = $senate_votes_helper->checkParagraphExists($existing_paragraph, $file_row);
 
                     if (empty($pid)) {
@@ -79,25 +82,56 @@ class SenateVotes extends QueueWorkerBase {
                           'target_revision_id' => $paragraph->getRevisionId(),
                         ];
                         $parent_node->save();
+                        $paragraphs['created'][] = $paragraph->id();
                       }
                     }
-//                    else {
+                    else {
+                      $paragraphs['updated'][] = $pid;
 //                      $paragraph = $senate_votes_helper->updateParagraph($pid, $file_row);
-//                    }
-                  }
-                  else {
-                    \Drupal::logger('senate_votes')
-                      ->error(__METHOD__ . ' ' . t('failed. Message = Parent node problem %file.', [
-                          '%file' => $file,
-                        ]));
+                    }
                   }
                 }
+
+//                $this->log($operation, $parent_node->id(), $paragraphs);
+              }
+              else {
+                \Drupal::logger('senate_votes')
+                  ->error(__METHOD__ . ' ' . t('failed. Message = Parent node problem %file.', [
+                      '%file' => $file,
+                    ]));
               }
             }
           }
 //          $senate_votes_helper->setFileStatus($file, $directory);
         }
 
+      }
+    }
+  }
+
+  public function log($operation, $nid, $paragraphs) {
+
+    if (!empty($operation) && !empty($nid) && !empty($paragraphs)) {
+      switch ($operation) {
+        case 'update':
+          \Drupal::logger('senate_votes')->notice(t('Node %nid has been updated. Paragraphs = <pre><code> %message </code></pre>', [
+            '%message' => print_r($paragraphs, TRUE),
+            '%nid' => $nid
+          ]));
+          break;
+        case 'create':
+          \Drupal::logger('senate_votes')->notice(t('Node %nid has been created. Paragraphs = <pre><code> %message </code></pre>', [
+            '%message' => print_r($paragraphs, TRUE),
+            '%nid' => $nid
+          ]));
+          break;
+        default:
+          \Drupal::logger('senate_votes')->notice(t('Node %nid operation: %op. Paragraphs = <pre><code> %message </code></pre>', [
+            '%message' => print_r($paragraphs, TRUE),
+            '%op' => $operation,
+            '%nid' => $nid
+          ]));
+          break;
       }
     }
   }
