d79f98b2e976a1290952680ef181d69dbd8d951f
dmitry.dombrovskiy@rockdevels.com <Karabas91234>
2020-07-20 14:15:07 +0300

    #117514 Add calendar to calendar page

--- slice/public/19-1642-Calendar-Journals.php
+++ slice/public/19-1642-Calendar-Journals.php
@@ -43,23 +43,30 @@
       <div class="bContainer">
 
 <!--        <div class="bSelect bSelect_a bSelect_size_d  bSelect_gap_b">-->
-          <form class="views-exposed-form bSelect bSelect_filter bSelect_a bSelect_size_d  bSelect_gap_b" data-drupal-selector="views-exposed-form-journals-page-1" action="/calendar/journals" method="get" id="views-exposed-form-journals-page-1" accept-charset="UTF-8">
-            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-year form-item-year form-no-label">
-              <select data-drupal-selector="edit-year" id="edit-year" name="year" class="form-select">
-                <option value="2019">2019</option>
-                <option value="2018" selected="selected">2018</option>
-              </select>
-            </div>
-            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-month form-item-month form-no-label">
-              <select data-drupal-selector="edit-month" id="edit-month" name="month" class="form-select">
-                <option value="02">February</option>
-                <option value="03" selected="selected">March</option>
-              </select>
-            </div>
-            <div data-drupal-selector="edit-actions" class="form-actions js-form-wrapper form-wrapper" id="edit-actions">
-              <input data-drupal-selector="edit-submit-journals" type="submit" id="edit-submit-journals" value="Apply" class="button js-form-submit form-submit"/>
-            </div>
-          </form>
+<!--          <form class="views-exposed-form bSelect bSelect_filter bSelect_a bSelect_size_d  bSelect_gap_b" data-drupal-selector="views-exposed-form-journals-page-1" action="/calendar/journals" method="get" id="views-exposed-form-journals-page-1" accept-charset="UTF-8">-->
+<!--            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-year form-item-year form-no-label">-->
+<!--              <select data-drupal-selector="edit-year" id="edit-year" name="year" class="form-select">-->
+<!--                <option value="2019">2019</option>-->
+<!--                <option value="2018" selected="selected">2018</option>-->
+<!--              </select>-->
+<!--            </div>-->
+<!--            <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-month form-item-month form-no-label">-->
+<!--              <select data-drupal-selector="edit-month" id="edit-month" name="month" class="form-select">-->
+<!--                <option value="02">February</option>-->
+<!--                <option value="03" selected="selected">March</option>-->
+<!--              </select>-->
+<!--            </div>-->
+<!--            <div data-drupal-selector="edit-actions" class="form-actions js-form-wrapper form-wrapper" id="edit-actions">-->
+<!--              <input data-drupal-selector="edit-submit-journals" type="submit" id="edit-submit-journals" value="Apply" class="button js-form-submit form-submit"/>-->
+<!--            </div>-->
+<!--          </form>-->
+
+        <div class="form bSelect bSelect_filter bSelect_a bSelect_gap_b bSelect_date">
+          <div class="form-item form-type-text form-item-date">
+<!--            <label class="form-required"></label>-->
+            <input type="text" class="form-text hasDatepicker" placeholder="Select Date">
+          </div>
+        </div>
 
 <!--        </div>-->
 
@@ -133,5 +140,22 @@
   </div>
   <?php include 'tpl/blocks/sFooter.inc'; ?>
 </div>
+
+<!--<div style="display: none">-->
+  <div id="ui-datepicker-div"
+       class="ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all"
+       style="position: absolute; top: 572px; left: 919px; z-index: 2; display: block;">
+    <div class="ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all">
+      <a class="ui-datepicker-prev ui-corner-all" data-handler="prev" data-event="click" title="Prev">
+        <span class="ui-icon ui-icon-circle-triangle-w">Prev</span></a>
+      <a class="ui-datepicker-next ui-corner-all ui-state-hover" data-handler="next" data-event="click" title="Next">
+        <span class="ui-icon ui-icon-circle-triangle-e">Next</span></a>
+      <div class="ui-datepicker-title">
+        <span class="ui-datepicker-month">January</span>&nbsp;
+        <span class="ui-datepicker-year">2018</span>
+      </div>
+    </div><table class="ui-datepicker-calendar"><thead><tr><th scope="col" class="ui-datepicker-week-end"><span title="Sunday">Su</span></th><th scope="col"><span title="Monday">Mo</span></th><th scope="col"><span title="Tuesday">Tu</span></th><th scope="col"><span title="Wednesday">We</span></th><th scope="col"><span title="Thursday">Th</span></th><th scope="col"><span title="Friday">Fr</span></th><th scope="col" class="ui-datepicker-week-end"><span title="Saturday">Sa</span></th></tr></thead><tbody><tr><td class=" ui-datepicker-week-end ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">1</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">2</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">3</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">4</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">5</a></td><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">6</a></td></tr><tr><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">7</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">8</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">9</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">10</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">11</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">12</a></td><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">13</a></td></tr><tr><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">14</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">15</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">16</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">17</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">18</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default ui-state-hover" href="#">19</a></td><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">20</a></td></tr><tr><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">21</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">22</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">23</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">24</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">25</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">26</a></td><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">27</a></td></tr><tr><td class=" ui-datepicker-week-end " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">28</a></td><td class=" ui-datepicker-days-cell-over  ui-datepicker-today" data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default ui-state-highlight ui-state-hover" href="#">29</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">30</a></td><td class=" " data-handler="selectDay" data-event="click" data-month="0" data-year="2018"><a class="ui-state-default" href="#">31</a></td><td class=" ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td><td class=" ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td><td class=" ui-datepicker-week-end ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td></tr></tbody></table></div>
+<!--</div>-->
+
 </body>
 </html>
