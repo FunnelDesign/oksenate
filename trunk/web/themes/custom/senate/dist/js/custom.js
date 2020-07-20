f36c254d497bb48bf48cd04c8b6267eb9b357cc6
diomid.tolkachev <diomid.tolkachev@rockdevels.com>
2020-07-20 20:28:29 +0300

    117514 default datepicker value

--- trunk/web/themes/custom/senate/dist/js/custom.js
+++ trunk/web/themes/custom/senate/dist/js/custom.js
@@ -1,58 +1,58 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.senateCustom = {
     attach: function (context, settings) {
-      $('#views-exposed-form-journals-page-1', context).once('journals-options').each(function () {
-        var yearElm = this.elements.item(0);
-        var monthElm = this.elements.item(1);
-        var daysElm = this.elements.item(2);
-        var options = drupalSettings.senate.view_journals || {};
-        var _this = this;
-        var $yearSelect = $('#views-exposed-form-journals-page-1 #edit-year + .select2');
-        var event = ($yearSelect.css('display') === 'none') ? 'change' : 'select2:select';
-        var year = yearElm ? yearElm.value : '';
-
-        $(yearElm).on(event, function (e) {
-          year = (event === 'select2:select') ? e.params.data.id : this.value;
-
-          var keys = Object.keys(options[year]);
-          keys.sort(function(a, b){
-            return parseInt(a) - parseInt(b);
-          });
-          monthElm.innerHTML = keys.map(function (key) {
-            return '<option value="' + key + '">' + options[year][key]['month'] + '</option>';
-          }).join();
-
-          var defaultMonth = keys[0];
-          var keysDay = Object.keys(options[year][defaultMonth]['days']);
-          keysDay.sort(function(a, b){
-            return parseInt(a) - parseInt(b);
-          });
-          daysElm.innerHTML = keysDay.map(function (key) {
-            return '<option value="' + key + '">' + options[year][defaultMonth]['days'][key] + '</option>';
-          }).join();
-          daysElm.innerHTML = '<option value="all">Day</option>' + daysElm.innerHTML;
-
-          _this.submit();
-        });
-
-        $(monthElm).on(event, function (e) {
-          var monthValue = (event === 'select2:select') ? e.params.data.id : this.value;
-
-          var keys = Object.keys(options[year][monthValue]['days']);
-          keys.sort(function(a, b){
-            return parseInt(a) - parseInt(b);
-          });
-          daysElm.innerHTML = keys.map(function (key) {
-            return '<option value="' + key + '">' + options[year][monthValue]['days'][key] + '</option>';
-          }).join();
-          daysElm.innerHTML = '<option value="all">Day</option>' + daysElm.innerHTML;
-          _this.submit();
-        });
-
-        $(daysElm).on(event, function (e) {
-          _this.submit();
-        });
-      });
+    //   $('#views-exposed-form-journals-page-1', context).once('journals-options').each(function () {
+    //     var yearElm = this.elements.item(0);
+    //     var monthElm = this.elements.item(1);
+    //     var daysElm = this.elements.item(2);
+    //     var options = drupalSettings.senate.view_journals || {};
+    //     var _this = this;
+    //     var $yearSelect = $('#views-exposed-form-journals-page-1 #edit-year + .select2');
+    //     var event = ($yearSelect.css('display') === 'none') ? 'change' : 'select2:select';
+    //     var year = yearElm ? yearElm.value : '';
+    //
+    //     $(yearElm).on(event, function (e) {
+    //       year = (event === 'select2:select') ? e.params.data.id : this.value;
+    //
+    //       var keys = Object.keys(options[year]);
+    //       keys.sort(function(a, b){
+    //         return parseInt(a) - parseInt(b);
+    //       });
+    //       monthElm.innerHTML = keys.map(function (key) {
+    //         return '<option value="' + key + '">' + options[year][key]['month'] + '</option>';
+    //       }).join();
+    //
+    //       var defaultMonth = keys[0];
+    //       var keysDay = Object.keys(options[year][defaultMonth]['days']);
+    //       keysDay.sort(function(a, b){
+    //         return parseInt(a) - parseInt(b);
+    //       });
+    //       daysElm.innerHTML = keysDay.map(function (key) {
+    //         return '<option value="' + key + '">' + options[year][defaultMonth]['days'][key] + '</option>';
+    //       }).join();
+    //       daysElm.innerHTML = '<option value="all">Day</option>' + daysElm.innerHTML;
+    //
+    //       _this.submit();
+    //     });
+    //
+    //     $(monthElm).on(event, function (e) {
+    //       var monthValue = (event === 'select2:select') ? e.params.data.id : this.value;
+    //
+    //       var keys = Object.keys(options[year][monthValue]['days']);
+    //       keys.sort(function(a, b){
+    //         return parseInt(a) - parseInt(b);
+    //       });
+    //       daysElm.innerHTML = keys.map(function (key) {
+    //         return '<option value="' + key + '">' + options[year][monthValue]['days'][key] + '</option>';
+    //       }).join();
+    //       daysElm.innerHTML = '<option value="all">Day</option>' + daysElm.innerHTML;
+    //       _this.submit();
+    //     });
+    //
+    //     $(daysElm).on(event, function (e) {
+    //       _this.submit();
+    //     });
+    //   });
 
       $('#views-exposed-form-search-statutes-constitutions-page-1', context).once('statutes').each(function () {
         var constitutions = this.elements['edit-constitutions'];
