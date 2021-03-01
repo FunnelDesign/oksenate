3ae6a90dc37a88c239a2cefbf59c7b7a28a0c51d
dmitry.dombrovskiy@rockdevels.com <karabas91234>
2021-03-01 16:23:00 +0300

    51103#comment-51103 Change senate journals

--- slice/public/19-1642-Calendar-Journals.php
+++ slice/public/19-1642-Calendar-Journals.php
@@ -64,6 +64,7 @@
         <div class="form bSelect bSelect_filter bSelect_a bSelect_gap_b bSelect_date">
           <div class="form-item form-type-text form-item-date">
 <!--            <label class="form-required"></label>-->
+            <input type="text" class="form-text datepicker-alt-filed" placeholder="AltField">
             <input type="text" class="form-text hasDatepicker" placeholder="Select Date">
           </div>
         </div>
