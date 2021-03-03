2c15d3f307b5ac0dc5b5959aa96a7815b3c6a095
dmitry.dombrovskiy@rockdevels.com <karabas91234>
2021-03-03 13:37:18 +0300

    #121096 Replicate page from house site

---
+++ slice/public/PublicMap.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'PublicMap'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_ind_j section_bg-color_c">
+
+      <div class="section__sNav section__sNav_a">
+
+        <div class="bContainer">
+          <a href="#" class="link link_cl_b link_back">back</a>
+          <div class="breadcrumb breadcrumb_a">
+            <a href="#">Redistricting</a>
+            <span>Map Submissions</span>
+          </div>
+          <?php include 'tpl/blocks/bShare-color-a.inc'; ?>
+        </div>
+
+      </div>
+
+      <div class="bContainer">
+
+        <div class="bTitle bTitle_wSub_a bTitle_gap_b">
+          <h1>Public Map Submissions</h1>
+        </div>
+        <div class="content-left">
+          The Oklahoma Legislature is excited to partner with Dave's Redistricting App (DRA) a free redistricting mapping resource that
+          Oklahomans can utilize to engage in the redistricting process and try their hand at drawing state legislative districts.
+          Dave's Redistricting App is free to use. Sign up and log in through the following link to create
+          an account: <a id="ctl00_ContentPlaceHolder1_hplDRA" title="Link to Dave's Redistricting App" href="https://davesredistricting.org/maps#home" target="_blank" style="color:#15212E;">https://davesredistricting.org/maps#home</a><br><br>
+
+          Oklahomans can draw a district map for either the 48-seat state Senate or the 101-seat state House.
+          Please consult the House Public Map Submission Parameters and DRA Partnership documents linked below for full details
+          and instructions on the public map submission process.<br><br>
+
+          <a id="ctl00_ContentPlaceHolder1_hplHousePubMap" title="2021 House Public Map Submission Parameters" href="../Documents/Districts/2021%20House%20Public%20Map%20Submission%20Parameters.pdf" target="_blank" style="color:#15212E;">House Public Map Submission Parameters</a><br><br>
+
+          <a id="ctl00_ContentPlaceHolder1_hplDRAPart" title="DRA Partnership" href="../Documents/Districts/DRA%20Partnership.pdf" target="_blank" style="color:#15212E;">DRA Partnership</a><br><br>
+
+
+          <b>IMPORTANT DATES:</b><br><br>
+
+          <b>MARCH 1, 6:00 P.M. – House and Senate Redistricting Office hosts virtual town hall</b><br>
+          <ul style="list-style-type:disc;">
+            <span id="ctl00_ContentPlaceHolder1_lblQA">Q&amp;A on public map submission process/parameters.</span>
+          </ul>
+          <ul style="list-style-type:disc;">
+            <span id="ctl00_ContentPlaceHolder1_lblDRA">A representative from DRA will host demo/training on app features, etc.</span>
+          </ul>
+
+
+          <b>APRIL 4, 5:00 P.M.– Deadline for public map submission (legislative only)</b><br><br>
+
+          <b>WEEK OF APRIL 12-15 – Public invited to present public map submissions to committee</b>
+
+        </div>
+
+
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
