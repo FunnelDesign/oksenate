bbc615fe1a807685fad4294487c8dacc45e5aac7
dmitry.dombrovskiy@rockdevels.com <karabas91234>
2021-04-15 11:12:13 +0300

    #121900 Develop pro tem page
    (cherry picked from commit 01d1f10)

---
+++ slice/public/Pro-Tem-Page.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'Pro-Tem-Page'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <?php include 'tpl/blocks/bShare-no-bread.inc'; ?>
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-21.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-50.png"
+                 srcset="../dist/images/titleIco/title-ico-50-2x.png 2x">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Pro Tem</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+        <h2>President Pro Tempore Appointments to <br> Agencies, Boards, and Commissions</h2>
+        <div class="bWrap bWrap_size_h bWrap_cl_a bWrap_f_a bWrap_f_a_17 bWrap_gap_b">
+          <p>The Senate President Pro Tempore makes apppointments to numerous state agencies, boards, and commissions (ABCs). These entities cover a huge variety of subjects including agriculture, education, health care, public safety, and many more.
+          </p>
+          <p>These entities have an important role in the oversight and regulation of important issues in Oklahoma, which is why President Pro Tempore Greg Treat is emphasizing appointments made by his office.
+          </p>
+          <p>Pro Tempore Treat is seeking qualified, energetic, enthusiastic Oklahomans willing to serve and share their talents and expertise for the advancement of our state.
+          </p>
+          <p>Here is a list of some of the upcoming appointments.</p>
+        </div>
+
+        <div class="bWrap bWrap_size_h bWrap_cl_a bWrap_f_a bWrap_f_a_17 bWrap_gap_b">
+          <div class="bList bList_a">
+            <ul>
+              <li>Judicial Nominating Commission</li>
+              <li>AThe State Council on Aging, someone 60 or over from the public or private sector.</li>
+              <li>Professional Responsibility Commission, a non-lawyer residing in Congressional District 3</li>
+              <li>Government Technology Application Review Board, Legislators and state employees prohibited from serving. </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="bWrap bWrap_size_h bWrap_cl_a bWrap_f_a bWrap_f_a_17 bWrap_gap_b">
+          <p>If you have an interest or expertise in these areas and would like to serve, 
+            or if you have questions about what other opportunities to serve
+            on ABCs there may be, please
+            <a href="mailto:Jamie.steele@oksenate.gov">click here to e-mail the Pro Tem’s Office</a>.
+          </p>
+          <p>You may also call Jamie Steele in the Pro Tempore’s Office at (405) 521-5771 for questions about the appointment process.</p>
+        </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
