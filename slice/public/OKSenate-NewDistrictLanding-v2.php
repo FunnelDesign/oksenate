b0bbc6465fe316b2b0fce6705317523edd1fd415
dmitry.dombrovskiy@rockdevels.com <karabas91234>
2022-01-10 12:05:28 +0300

    #126488 NEW DISTRICT PAGE

---
+++ slice/public/OKSenate-NewDistrictLanding-v2.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'OKSenate-NewDistrictLanding-v2'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <?php include 'tpl/blocks/bShare-no-bread.inc'; ?>
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-13.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-57.png"
+                 srcset="../dist/images/titleIco/title-ico-57-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>District Maps</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+      <div class="bContainer">
+        <div class="b-district-maps">
+          <div class="b-district-maps__nav">
+            <a href="#l-current-senate" class="b-district-maps__nav-item hashLink">
+              <span>
+                Current Senate <br>
+                District maps <br>
+                <b>2012-2021</b>
+              </span>
+            </a>
+            <a href="#l-new-senate" class="b-district-maps__nav-item hashLink">
+              <span>
+                New Senate <br>
+                District maps <br>
+                <b>2022-2031</b>
+              </span>
+            </a>
+          </div>
+        </div>
+        
+        <div class="b-district-maps__group">
+          <div class="b-district-maps__item" id="current-senate">
+            <div class="b-district-maps__item-title">
+              <h2>Current Senate Maps</h2>
+              <h6>2012-2021</h6>
+            </div>
+
+            <div class="b-district-maps__item-group">
+
+              <div class="b-district-maps__list">
+                <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+                  <div class="bListLinks__links">
+                    <a href="#" class="bListLinks__link">District 1
+                    </a>
+                    <a href="#" class="bListLinks__link">District 2
+                    </a>
+                    <a href="#" class="bListLinks__link">District 3
+                    </a>
+                    <a href="#" class="bListLinks__link">District 4
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="b-district-maps__list">
+                <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+                  <div class="bListLinks__links">
+                    <a href="#" class="bListLinks__link">District 1
+                    </a>
+                    <a href="#" class="bListLinks__link">District 2
+                    </a>
+                    <a href="#" class="bListLinks__link">District 3
+                    </a>
+                    <a href="#" class="bListLinks__link">District 4
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="b-district-maps__item" id="new-senate">
+            <div class="b-district-maps__item-title">
+              <h2>New Senate Maps</h2>
+              <h6>2022-2031</h6>
+            </div>
+
+            <div class="b-district-maps__item-group">
+
+              <div class="b-district-maps__list">
+                <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+                  <div class="bListLinks__links">
+                    <a href="#" class="bListLinks__link">District 1
+                    </a>
+                    <a href="#" class="bListLinks__link">District 2
+                    </a>
+                    <a href="#" class="bListLinks__link">District 3
+                    </a>
+                    <a href="#" class="bListLinks__link">District 4
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="b-district-maps__list">
+                <div class="bListLinks bListLinks_btnArr bListLinks_b bListLinks_gap_a">
+                  <div class="bListLinks__links">
+                    <a href="#" class="bListLinks__link">District 1
+                    </a>
+                    <a href="#" class="bListLinks__link">District 2
+                    </a>
+                    <a href="#" class="bListLinks__link">District 3
+                    </a>
+                    <a href="#" class="bListLinks__link">District 4
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+          
+        </div>
+
+  
+      </div>
+    </section>
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
