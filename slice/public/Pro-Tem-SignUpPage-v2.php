4a940f66207460217a87f641f6e17024129251c7
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2023-01-04 15:45:35 +0300

    #132645 Develop Press Release Sign Up page

---
+++ slice/public/Pro-Tem-SignUpPage-v2.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Contacts'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+      <?php include 'tpl/blocks/bShare-no-bread.inc'; ?>
+      <div class="section__bg-wrap">
+        <div class="section__bg" style="background-image: url('../dist/images/tmp/section-bg-img-21.jpg')"></div>
+      </div>
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-20.png" srcset="../dist/images/titleIco/title-ico-20-2x.png 2x"
+                 alt="">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>Press Release Sign Up</h1>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <div class="bTiles bTiles_rel bTiles_rel_h">
+
+          <div class="bTiles__items">
+            <span class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/titleIco/title-ico-61.png" srcset="../dist/images/titleIco/title-ico-61-2x.png 2x" alt="">
+              </span>
+              <span class="bTiles__title">
+                Pro Tem Press Release
+              </span>
+              <span class="bTiles__btnWrap">
+                <a class="btn btn_s_d" href="#">
+                  SIGN UP
+                </a>
+              </span>
+
+            </span>
+
+            <span class="bTiles__item">
+              <span class="bTiles__ico">
+                <img src="../dist/images/titleIco/title-ico-62.png" srcset="../dist/images/titleIco/title-ico-62-2x.png 2x" alt="">
+              </span>
+              <span class="bTiles__title">
+                Members Press Release
+              </span>
+              <span class="bTiles__btnWrap">
+                <a class="btn btn_s_d" href="#">
+                  SIGN UP
+                </a>
+              </span>
+
+            </span>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
