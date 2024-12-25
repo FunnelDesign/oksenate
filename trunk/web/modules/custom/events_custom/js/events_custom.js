136a6f1b34d858451248563dc54742010595dd61
dmitry.drobyshevsky <drobyshevsky.d@gmail.com>
2024-12-25 17:19:43 +0300

    update, d9 to 10, once lib

--- trunk/web/modules/custom/events_custom/js/events_custom.js
+++ trunk/web/modules/custom/events_custom/js/events_custom.js
@@ -4,11 +4,11 @@
   Drupal.behaviors.eventsCustom = {
     attach: function (context, settings) {
 
-      $('.events-holder', context).once('events-holder').each(function(){
+      $(once('events-holder', '.events-holder', context)).each(function(){
 
         Drupal.ajax({url: '/events-custom/insert'}).execute();
       });
-      $('.events-home-holder', context).once('events-holder').each(function(){
+      $(once('events-holder','.events-home-holder', context)).each(function(){
 
         Drupal.ajax({url: '/events-custom/insert-home'}).execute();
       });
