cb18fe9e22d03df3e8615e61e48287ca2b277f5a
dmitry.dombrovskiy <dmitry.dombrovskiy@rockdevels.com>
2025-02-18 15:35:56 +0300

    General Order Calendar

---
+++ slice/public/Search-Statutes-Constitutions-2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<?php $title = '19-1642-Publications-LegislativeBriefs'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="page">
+<div class="pageWr">
+  <?php include 'tpl/blocks/sHeader.inc'; ?>
+  <div class="pageIn">
+
+    <section class="section section_bg-color_f section_inner-v-centered">
+<!--      <div class="section__sNav">-->
+<!---->
+<!--        <div class="bContainer">-->
+<!--          <a href="#" class="link link_cl_a link_back">back</a>-->
+<!--          <div class="breadcrumb">-->
+<!--            <a href="#">Publications</a>-->
+<!--            <span>Legislative Briefs</span>-->
+<!--          </div>-->
+<!--        </div>-->
+<!---->
+<!--      </div>-->
+
+      <?php include 'tpl/blocks/bShare-no-bread.inc'; ?>
+
+      <div class="section__v-inner section__v-inner_d">
+        <div class="bContainer">
+
+          <div class="pageIn__ico">
+            <img src="../dist/images/titleIco/title-ico-14.png"
+                 srcset="../dist/images/titleIco/title-ico-14-2x.png 2x">
+          </div>
+
+          <div class="bTitle bTitle_wSub_a">
+            <h1>General Order Calendar</h1>
+            <div class="bTitle__sub bTitle__sub_a bTitle__sub_cl_a">
+              <p><strong>(Senate Rule 8-22.A.4.)</strong><br>
+              2025 Regular Session</p>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section section_ind_c section_bg-color_c">
+
+      <div class="bContainer">
+
+        <iframe
+                scrolling="yes"
+                src="http://www.oklegislature.gov/sgeneralorder.aspx"
+                marginwidth="1"
+                marginheight="0"
+                target="_self"
+                frameborder="0"
+                style="width: 100%;height: 4000px;border: none;max-width: 900px;margin-inline: auto;display: block;"></iframe>
+
+      </div>
+
+    </section>
+
+
+  </div>
+  <?php include 'tpl/blocks/sFooter.inc'; ?>
+</div>
+</body>
+</html>
